a1017783f38ddd4b34f0b6daaa138c5741fff540
Olivier Marsden <Olivier.Marsden@ecmwf.int>
2022-02-07 21:02:05 +0000

    CUDA-aware MPI - 1 : clean baseline replicating standard code path.
    For sake of comparing, added _CA_ (CUDA-aware MPI) libraries and binaries,
    down the line we will probably want to make CUDA-aware MPI the default.

--- src/trans/gpu/internal/trltom_mod.F90
+++ src/trans/gpu/internal/trltom_mod.F90
@@ -9,6 +9,156 @@
 
 MODULE TRLTOM_MOD
   CONTAINS
+  SUBROUTINE TRLTOM_CUDAAWARE(PFBUF_IN,PFBUF,KFIELD)
+  
+  !**** *TRLTOM * - transposition in Fourierspace
+  
+  !     Purpose.
+  !     --------
+  !              Transpose Fourier coefficients from partitioning
+  !              over latitudes to partitioning over wave numbers
+  !              This is done between inverse Legendre Transform
+  !              and inverse FFT.
+  !              This is the inverse routine of TRMTOL.
+  
+  !**   Interface.
+  !     ----------
+  !        *CALL* *TRLTOM(...)*
+  
+  !        Explicit arguments : PFBUF  - Fourier coefficient buffer. It is
+  !        --------------------          used for both input and output.
+  
+  !                             KFIELD - Number of fields communicated
+  
+  !        Implicit arguments :
+  !        --------------------
+  
+  !     Method.
+  !     -------
+  !        See documentation
+  
+  !     Externals.
+  !     ----------
+  
+  !     Reference.
+  !     ----------
+  !        ECMWF Research Department documentation of the IFS
+  
+  !     Author.
+  !     -------
+  !        MPP Group *ECMWF*
+  
+  !     Modifications.
+  !     --------------
+  !        Original : 95-10-01
+  !        Modified : 97-06-18 G. Mozdzynski - control MPI mailbox use
+  !                                            (NCOMBFLEN) for nphase.eq.1
+  !        Modified : 99-05-28  D.Salmond - Optimise copies.
+  !        Modified : 00-02-02  M.Hamrud  - Remove NPHASE
+  !        D.Salmond : 01-11-23 LIMP_NOOLAP Option for non-overlapping message
+  !                             passing and buffer packing
+  !        G.Mozdzynski : 08-01-01 Cleanup
+  !        Y.Seity   : 07-08-30 Add barrier synchonisation under LSYNC_TRANS
+  !     ------------------------------------------------------------------
+  
+  USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRBT
+  USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
+  
+  USE MPL_MODULE  ,ONLY : MPL_ALLTOALLV, MPL_BARRIER, MPL_ALL_MS_COMM, MPL_MYRANK, MPL_WAIT, JP_NON_BLOCKING_STANDARD
+  
+  USE TPM_DISTR       ,ONLY : D, MTAGLM, MYSETW, NPRTRW, NPROC, MYPROC
+  USE TPM_GEN         ,ONLY : LSYNC_TRANS
+  
+  USE MPI
+  
+  !USE SET2PE_MOD
+  !USE MYSENDSET_MOD
+  !USE MYRECVSET_MOD
+  !USE ABORT_TRANS_MOD
+  !
+  
+  IMPLICIT NONE
+  
+  
+  INTERFACE
+  
+    FUNCTION ALLTOALLV_CUDAIPC(input,len,soff,output,roff,mtol_or_ltom) BIND(C,name='Alltoallv_CUDAIPC')
+      USE, INTRINSIC :: ISO_C_BINDING
+      IMPLICIT NONE
+      real(c_double), dimension(*) :: input,output
+      integer(c_int), dimension(*) :: len,soff,roff
+      integer(c_int),value :: mtol_or_ltom
+      integer(c_int) :: ALLTOALLV_CUDAIPC
+    END FUNCTION ALLTOALLV_CUDAIPC
+  
+  END INTERFACE
+  
+  
+  INTEGER(KIND=JPIM),INTENT(IN)  :: KFIELD
+  REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF(:)
+  REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF_IN(:)
+  
+  INTEGER(KIND=JPIM) :: ILENS(NPRTRW),IOFFS(NPRTRW),ILENR(NPRTRW),IOFFR(NPRTRW)
+  
+  INTEGER(KIND=JPIM) :: ITAG, J, ILEN, ISTA
+  
+REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
+REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR
+REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR2
+  
+  REAL(KIND=JPRBT)    :: ZDUM(1)
+  INTEGER(KIND=JPIM) :: IREQ
+  INTEGER(KIND=JPIM) :: IERROR
+  !     ------------------------------------------------------------------
+  
+  REAL(KIND=JPRBT) :: T1, T2, TIMEF, Tc
+  INTEGER(KIND=JPIM) :: MTOL_OR_LTOM, NOFULLPEERACCESS
+  INTEGER(KIND=JPIM) :: IRANK,IUNIT
+
+
+  IF (LHOOK) CALL DR_HOOK('TRLTOM_CUDAAWARE',0,ZHOOK_HANDLE)
+
+#ifdef PARKINDTRANS_SINGLE
+#define TRLTOM_DTYPE MPI_REAL
+#else
+#define TRLTOM_DTYPE MPI_DOUBLE_PRECISION
+#endif
+  
+  ITAG = MTAGLM
+  
+  DO J=1,NPRTRW
+    ILENS(J) = D%NLTSGTB(J)*KFIELD
+    IOFFS(J) = D%NSTAGT1B(D%MSTABF(J))*KFIELD
+    ILENR(J) = D%NLTSFTB(J)*KFIELD
+    IOFFR(J) = D%NSTAGT1B(J)*KFIELD
+  ENDDO
+  
+  IF(NPROC > 1) THEN
+    CALL GSTATS(806,0)
+  
+!!    CALL MPL_ALLTOALLV(PSENDBUF=PFBUF_IN,KSENDCOUNTS=ILENS,&
+!!     & PRECVBUF=PFBUF,KRECVCOUNTS=ILENR,KSENDDISPL=IOFFS,KRECVDISPL=IOFFR,&
+!!     & KCOMM=MPL_ALL_MS_COMM,CDSTRING='TRLTOM:')
+    CALL MPI_ALLTOALLV(PFBUF_IN,ILENS,IOFFS,TRLTOM_DTYPE,&
+     & PFBUF,ILENR,IOFFR, TRLTOM_DTYPE, &
+     & MPL_ALL_MS_COMM, IERROR)
+
+    CALL MPI_BARRIER(MPI_COMM_WORLD,IERROR)
+    CALL GSTATS(806,1)
+  ELSE
+    ILEN = D%NLTSGTB(MYSETW)*KFIELD
+    ISTA = D%NSTAGT1B(MYSETW)*KFIELD+1
+    CALL GSTATS(1607,0)
+    DO J=ISTA,ISTA+ILEN-1
+      PFBUF(J) = PFBUF_IN(J)
+    ENDDO
+    CALL GSTATS(1607,1)
+  ENDIF
+  
+  IF (LHOOK) CALL DR_HOOK('TRLTOM_CUDAAWARE',1,ZHOOK_HANDLE)
+  !     ------------------------------------------------------------------
+  END SUBROUTINE TRLTOM_CUDAAWARE
+
   SUBROUTINE TRLTOM(PFBUF_IN,PFBUF,KFIELD)
   
   !**** *TRLTOM * - transposition in Fourierspace
