39387b6371926a934a0666310b997fdd4351d08d
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:48:51 -0700

    Cleanup uvtvd
    * note vordiv gives wrong results with Cray

--- src/trans/gpu/internal/uvtvd_mod.F90
+++ src/trans/gpu/internal/uvtvd_mod.F90
@@ -1,5 +1,6 @@
 ! (C) Copyright 1991- ECMWF.
 ! (C) Copyright 1991- Meteo-France.
+! (C) Copyright 2022- NVIDIA.
 ! 
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -10,8 +11,7 @@
 
 MODULE UVTVD_MOD
 CONTAINS
-SUBROUTINE UVTVD(KFIELD)
-!SUBROUTINE UVTVD(KFIELD,PEPSNM,PU,PV,PVOR,PDIV)
+SUBROUTINE UVTVD(KF_UV)
 
 !**** *UVTVD* - Compute vor/div from u and v in spectral space
 
@@ -23,19 +23,10 @@ SUBROUTINE UVTVD(KFIELD)
 
 !**   Interface.
 !     ----------
-!        CALL UVTVD(KM,KFIELD,PEPSNM,PU,PV,PVOR,PDIV)
+!        CALL UVTVD(KF_UV)
 
-!        Explicit arguments :  KM - zonal wave-number
-!        --------------------  KFIELD - number of fields (levels)
-!                              PEPSNM - REPSNM for wavenumber KM
-!                              PU - u wind component for zonal
-!                                   wavenumber KM
-!                              PV - v wind component for zonal
-!                                   wavenumber KM
-!                              PVOR - vorticity for zonal
-!                                     wavenumber KM
-!                              PDIV - divergence for zonal
-!                                     wavenumber KM
+!        Explicit arguments :  
+!        --------------------  KF_UV - number of fields (levels)
 
 
 !     Method.  See ref.
@@ -69,30 +60,25 @@ USE TPM_FIELDS      ,ONLY : ZOA1,ZOA2,ZEPSNM
 IMPLICIT NONE
 
 !     DUMMY INTEGER SCALARS
-INTEGER(KIND=JPIM), INTENT(IN)  :: KFIELD
+INTEGER(KIND=JPIM), INTENT(IN)  :: KF_UV
 INTEGER(KIND=JPIM)  :: KM, KMLOC
-
-!REAL(KIND=JPRBT), INTENT(IN)     :: PEPSNM(1:d%nump,0:R%NTMAX+2)
-!REAL(KIND=JPRBT), INTENT(OUT)    :: PVOR(:,:,:),PDIV(:,:,:)
-!REAL(KIND=JPRBT), INTENT(INOUT)  :: PU  (:,:,:),PV  (:,:,:)
 
 !     LOCAL INTEGER SCALARS
 INTEGER(KIND=JPIM) :: II, IN, IR, J, JN, ITMAX
 INTEGER(KIND=JPIM) :: IUS, IUE, IVS, IVE, IVORS, IVORE, IDIVS, IDIVE
 
 !     LOCAL REAL SCALARS
-REAL(KIND=JPRBT) :: ZKM
-REAL(KIND=JPRBT) :: ZN(-1:R%NTMAX+3)
+REAL(KIND=JPRBT) :: ZKM,ZJN
 REAL(KIND=JPRBT), POINTER :: PU(:,:,:),PV(:,:,:),PVOR(:,:,:),PDIV(:,:,:)
 
 IUS = 1
-IUE = 2*KFIELD
-IVS = 2*KFIELD+1
-IVE = 4*KFIELD
+IUE = 2*KF_UV
+IVS = 2*KF_UV+1
+IVE = 4*KF_UV
 IVORS = 1
-IVORE = 2*KFIELD
-IDIVS = 2*KFIELD+1
-IDIVE = 4*KFIELD
+IVORE = 2*KF_UV
+IDIVS = 2*KF_UV+1
+IDIVE = 4*KF_UV
 
 !     ------------------------------------------------------------------
 
@@ -106,94 +92,88 @@ PDIV => ZOA2(IDIVS:IDIVE,:,:)
 
 #ifdef ACCGPU
 !$ACC DATA &
-!$ACC& CREATE(ZN) &
-!$ACC& PRESENT(D_MYMS,D_NUMP,R_NTMAX,F_RN,ZEPSNM,PU,PV,PVOR,PDIV)
+!$ACC& PRESENT(D_MYMS,D_NUMP,R_NTMAX,F_RN) &
+!$ACC& PRESENT(ZEPSNM,PU,PV,PVOR,PDIV)
 #endif
 #ifdef OMPGPU
 !WARNING: following line should be PRESENT,ALLOC but causes issues with AMD compiler!
 !$OMP TARGET DATA&
-!$OMP& MAP(ALLOC:ZN) &
 !$OMP& MAP(TO:D_MYMS,D_NUMP,R_NTMAX) &
 !$OMP& MAP(TO:F_RN) &
 !$OMP& MAP(ALLOC:ZEPSNM,PU,PV,PVOR,PDIV)
 #endif
 
-#ifdef OMPGPU
-!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO
-!! DEFAULT(NONE) SHARED(R_NTMAX,ZN,F_RN)
-#endif
-#ifdef ACCGPU
-!$ACC PARALLEL LOOP DEFAULT(NONE) PRESENT(R_NTMAX,ZN,F_RN)
-#endif
-DO J=-1,R_NTMAX+3
-  ZN(j) = F_RN(j)
-ENDDO
 !*       1.1      SET N=KM-1 COMPONENT TO 0 FOR U AND V
 
 #ifdef OMPGPU
 !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2)
 !! PRIVATE(KM,IN) DEFAULT(NONE) &
-!!$OMP& SHARED(D_NUMP,KFIELD,D_MYMS,PU,PV)
+!!$OMP& SHARED(D_NUMP,KF_UV,D_MYMS,PU,PV)
 #endif
 #ifdef ACCGPU
-!$ACC PARALLEL LOOP COLLAPSE(2) PRIVATE(KM,IN) DEFAULT(NONE) &
-!$ACC& COPYIN(KFIELD) &
+!$ACC PARALLEL LOOP COLLAPSE(2) PRIVATE(KM) DEFAULT(NONE) &
+!$ACC& COPYIN(KF_UV) &
 !$ACC& PRESENT(D_NUMP,D_MYMS,F_RN,PU,PV,R_NTMAX)
 #endif
 DO KMLOC=1,D_NUMP
-  DO J=1,2*KFIELD
+  DO J=1,2*KF_UV
     KM = D_MYMS(KMLOC)
-    !IN = F%NLTN(KM-1)
-    IN=R_NTMAX+3-KM
-    PU(J,IN,KMLOC) = 0.0_JPRBT
-    PV(J,IN,KMLOC) = 0.0_JPRBT
+    PU(J,R_NTMAX+3-KM,KMLOC) = 0.0_JPRBT
+    PV(J,R_NTMAX+3-KM,KMLOC) = 0.0_JPRBT
   ENDDO
 ENDDO
 
 !*       1.2      COMPUTE VORTICITY AND DIVERGENCE.
 
 #ifdef OMPGPU
-!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(3) PRIVATE(IR,II,IN,KM,ZKM) DEFAULT(NONE) &
-!$OMP& SHARED(D_NUMP,R_NTMAX,KFIELD,D_MYMS,PVOR,PV,PU,ZN,PDIV,ZEPSNM)
+!$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2) PRIVATE(IR,II,IN,KM,ZKM,JN,ZJN) DEFAULT(NONE) &
+!$OMP& SHARED(D_NUMP,R_NTMAX,KF_UV,D_MYMS,PVOR,PV,PU,PDIV,ZEPSNM)
 #endif
 #ifdef ACCGPU
-!$ACC PARALLEL LOOP COLLAPSE(3) PRIVATE(IR,II,IN,KM,ZKM) DEFAULT(NONE) &
-!$ACC& COPYIN(KFIELD) &
-!$ACC& PRESENT(D_NUMP,R_NTMAX,D_MYMS,PVOR,PV,PU,ZN,PDIV,ZEPSNM)
+!$ACC PARALLEL LOOP COLLAPSE(2) PRIVATE(IR,II,IN,KM,ZKM,JN,ZJN) DEFAULT(NONE) &
+!$ACC& COPYIN(KF_UV) &
+!$ACC& PRESENT(D_NUMP,R_NTMAX,D_MYMS,PVOR,PV,PU,PDIV,ZEPSNM)
 #endif
 DO KMLOC=1,D_NUMP
-  DO JN=0,R_NTMAX
-    DO J=1,KFIELD
-      IR = 2*J-1
-      II = IR+1
-      KM = D_MYMS(KMLOC)
-      ZKM = REAL(KM,JPRBT)
-      IN = R_NTMAX+2-JN
+  DO J=1,KF_UV
+    IR = 2*J-1
+    II = IR+1
+    KM = D_MYMS(KMLOC)
+    ZKM = REAL(KM,JPRBT)
 
-      IF(KM /= 0 .AND. JN.GE.KM) THEN
-      PVOR(IR,IN,KMLOC) = -ZKM*PV(II,IN,KMLOC)-&
-       &ZN(JN)*ZEPSNM(KMLOC,JN+1)*PU(IR,IN-1,KMLOC)+&
-       &ZN(JN+1)*ZEPSNM(KMLOC,JN)*PU(IR,IN+1,KMLOC)
-      PVOR(II,IN,KMLOC) = +ZKM*PV(IR,IN,KMLOC)-&
-       &ZN(JN)*ZEPSNM(KMLOC,JN+1)*PU(II,IN-1,KMLOC)+&
-       &ZN(JN+1)*ZEPSNM(KMLOC,JN)*PU(II,IN+1,KMLOC)
-      PDIV(IR,IN,KMLOC) = -ZKM*PU(II,IN,KMLOC)+&
-       &ZN(JN)*ZEPSNM(KMLOC,JN+1)*PV(IR,IN-1,KMLOC)-&
-       &ZN(JN+1)*ZEPSNM(KMLOC,JN)*PV(IR,IN+1,KMLOC)
-      PDIV(II,IN,KMLOC) = +ZKM*PU(IR,IN,KMLOC)+&
-       &ZN(JN)*ZEPSNM(KMLOC,JN+1)*PV(II,IN-1,KMLOC)-&
-       &ZN(JN+1)*ZEPSNM(KMLOC,JN)*PV(II,IN+1,KMLOC)
-      ELSE
-        IF(KM == 0) THEN
-         PVOR(IR,IN,KMLOC) = -&
-         &ZN(JN)*ZEPSNM(KMLOC,JN+1)*PU(IR,IN-1,KMLOC)+&
-         &ZN(JN+1)*ZEPSNM(KMLOC,JN)*PU(IR,IN+1,KMLOC)
-         PDIV(IR,IN,KMLOC) = &
-         &ZN(JN)*ZEPSNM(KMLOC,JN+1)*PV(IR,IN-1,KMLOC)-&
-         &ZN(JN+1)*ZEPSNM(KMLOC,JN)*PV(IR,IN+1,KMLOC)
-        ENDIF
-      ENDIF
-   ENDDO
+    IF(KM /= 0) THEN
+      !$ACC LOOP SEQ
+      DO JN=KM,R_NTMAX
+        IN = R_NTMAX+2-JN
+        ZJN = JN
+
+        PVOR(IR,IN,KMLOC) = -ZKM*PV(II,IN,KMLOC)-&
+         &ZJN*ZEPSNM(KMLOC,JN+1)*PU(IR,IN-1,KMLOC)+&
+         &(ZJN+1)*ZEPSNM(KMLOC,JN)*PU(IR,IN+1,KMLOC)
+        PVOR(II,IN,KMLOC) = +ZKM*PV(IR,IN,KMLOC)-&
+         &ZJN*ZEPSNM(KMLOC,JN+1)*PU(II,IN-1,KMLOC)+&
+         &(ZJN+1)*ZEPSNM(KMLOC,JN)*PU(II,IN+1,KMLOC)
+        PDIV(IR,IN,KMLOC) = -ZKM*PU(II,IN,KMLOC)+&
+         &ZJN*ZEPSNM(KMLOC,JN+1)*PV(IR,IN-1,KMLOC)-&
+         &(ZJN+1)*ZEPSNM(KMLOC,JN)*PV(IR,IN+1,KMLOC)
+        PDIV(II,IN,KMLOC) = +ZKM*PU(IR,IN,KMLOC)+&
+         &ZJN*ZEPSNM(KMLOC,JN+1)*PV(II,IN-1,KMLOC)-&
+         &(ZJN+1)*ZEPSNM(KMLOC,JN)*PV(II,IN+1,KMLOC)
+      ENDDO
+    ELSE
+      !$ACC LOOP SEQ
+      DO JN=0,R_NTMAX
+        IN = R_NTMAX+2-JN
+        ZJN = JN
+
+        PVOR(IR,IN,KMLOC) = -&
+         &ZJN*ZEPSNM(KMLOC,JN+1)*PU(IR,IN-1,KMLOC)+&
+         &(ZJN+1)*ZEPSNM(KMLOC,JN)*PU(IR,IN+1,KMLOC)
+        PDIV(IR,IN,KMLOC) = &
+         &ZJN*ZEPSNM(KMLOC,JN+1)*PV(IR,IN-1,KMLOC)-&
+         &(ZJN+1)*ZEPSNM(KMLOC,JN)*PV(IR,IN+1,KMLOC)
+      ENDDO
+    ENDIF
   ENDDO
 ENDDO
 #ifdef OMPGPU
