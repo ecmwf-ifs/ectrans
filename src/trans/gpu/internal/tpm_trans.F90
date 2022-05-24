fa2c8888ce735d56d680812119edd5e7aabb9ddd
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:49:35 -0700

    share reuse_ptr

--- src/trans/gpu/internal/tpm_trans.F90
+++ src/trans/gpu/internal/tpm_trans.F90
@@ -70,7 +70,7 @@ REAL(KIND=JPRBT),ALLOCATABLE  :: ZMAXGPN(:)
 ! we adapt the size. After 2 iterations this should lead to constant runtimes
 ! (the first iteration is used to get the max buffer size, the second iteration
 ! is going to recreate the graphs if needed)
-REAL(KIND=JPRBT),ALLOCATABLE,TARGET  :: REUSE_PTR(:)
+REAL(KIND=JPRBT),POINTER :: REUSE_PTR(:)
 
 !!! with Cray compiler this creates a lot more problems than good ... , Nils
 !!!$ACC DECLARE CREATE(FOUBUF_IN)
