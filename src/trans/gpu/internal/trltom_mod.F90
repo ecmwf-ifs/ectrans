60a0e8ef4a31d2160b4ad2fc70aa84ec5d5e6c40
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:48:57 -0700

    Changes for TRMTOL
    - TRMTOL call takes ZGTF_START now
    - trltom and trmtol do exactly the same now

--- src/trans/gpu/internal/trltom_mod.F90
+++ src/trans/gpu/internal/trltom_mod.F90
@@ -9,295 +9,250 @@
 !
 
 MODULE TRLTOM_MOD
-  CONTAINS
-  SUBROUTINE TRLTOM_CUDAAWARE(PFBUF_IN,PFBUF,KF_FS)
-  
-  !**** *TRLTOM * - transposition in Fourierspace
-  
-  !     Purpose.
-  !     --------
-  !              Transpose Fourier coefficients from partitioning
-  !              over latitudes to partitioning over wave numbers
-  !              This is done between inverse Legendre Transform
-  !              and inverse FFT.
-  !              This is the inverse routine of TRMTOL.
-  
-  !**   Interface.
-  !     ----------
-  !        *CALL* *TRLTOM(...)*
-  
-  !        Explicit arguments : PFBUF  - Fourier coefficient buffer. It is
-  !        --------------------          used for both input and output.
-  
-  !                             KF_FS - Number of fields communicated
-  
-  !        Implicit arguments :
-  !        --------------------
-  
-  !     Method.
-  !     -------
-  !        See documentation
-  
-  !     Externals.
-  !     ----------
-  
-  !     Reference.
-  !     ----------
-  !        ECMWF Research Department documentation of the IFS
-  
-  !     Author.
-  !     -------
-  !        MPP Group *ECMWF*
-  
-  !     Modifications.
-  !     --------------
-  !        Original : 95-10-01
-  !        Modified : 97-06-18 G. Mozdzynski - control MPI mailbox use
-  !                                            (NCOMBFLEN) for nphase.eq.1
-  !        Modified : 99-05-28  D.Salmond - Optimise copies.
-  !        Modified : 00-02-02  M.Hamrud  - Remove NPHASE
-  !        D.Salmond : 01-11-23 LIMP_NOOLAP Option for non-overlapping message
-  !                             passing and buffer packing
-  !        G.Mozdzynski : 08-01-01 Cleanup
-  !        Y.Seity   : 07-08-30 Add barrier synchonisation under LSYNC_TRANS
-  !     ------------------------------------------------------------------
-  
-  USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRBT
-  USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
-  
-  USE MPL_MODULE  ,ONLY : MPL_ALLTOALLV, MPL_BARRIER, MPL_ALL_MS_COMM, MPL_MYRANK, MPL_WAIT, JP_NON_BLOCKING_STANDARD
-  
-  USE TPM_DISTR       ,ONLY : D, MTAGLM, MYSETW, NPRTRW, NPROC, MYPROC
-  USE TPM_GEN         ,ONLY : LSYNC_TRANS
-  
-  USE MPI
-  
-  !USE SET2PE_MOD
-  !USE MYSENDSET_MOD
-  !USE MYRECVSET_MOD
-  !USE ABORT_TRANS_MOD
-  !
-  
-  IMPLICIT NONE
-  
-  INTEGER(KIND=JPIM),INTENT(IN)  :: KF_FS
-  REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF(:)
-  REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF_IN(:)
-  
-  INTEGER(KIND=JPIM) :: ILENS(NPRTRW),IOFFS(NPRTRW),ILENR(NPRTRW),IOFFR(NPRTRW)
-  
-  INTEGER(KIND=JPIM) :: ITAG, J, ILEN, ISTA
-  
+
+CONTAINS
+SUBROUTINE TRLTOM_CUDAAWARE(PFBUF_IN,PFBUF,KF_FS)
+
+!**** *TRLTOM * - transposition in Fourierspace
+
+!     Purpose.
+!     --------
+!              Transpose Fourier coefficients from partitioning
+!              over latitudes to partitioning over wave numbers
+!              This is done between inverse Legendre Transform
+!              and inverse FFT.
+!              This is the inverse routine of TRMTOL.
+
+!**   Interface.
+!     ----------
+!        *CALL* *TRLTOM(...)*
+
+!        Explicit arguments : PFBUF  - Fourier coefficient buffer. It is
+!        --------------------          used for both input and output.
+
+!                             KF_FS - Number of fields communicated
+
+!        Implicit arguments :
+!        --------------------
+
+!     Method.
+!     -------
+!        See documentation
+
+!     Externals.
+!     ----------
+
+!     Reference.
+!     ----------
+!        ECMWF Research Department documentation of the IFS
+
+!     Author.
+!     -------
+!        MPP Group *ECMWF*
+
+!     Modifications.
+!     --------------
+!        Original : 95-10-01
+!        Modified : 97-06-18 G. Mozdzynski - control MPI mailbox use
+!                                            (NCOMBFLEN) for nphase.eq.1
+!        Modified : 99-05-28  D.Salmond - Optimise copies.
+!        Modified : 00-02-02  M.Hamrud  - Remove NPHASE
+!        D.Salmond : 01-11-23 LIMP_NOOLAP Option for non-overlapping message
+!                             passing and buffer packing
+!        G.Mozdzynski: 08-01-01 Cleanup
+!        Y.Seity   : 07-08-30 Add barrier synchronisation under LSYNC_TRANS
+!     ------------------------------------------------------------------
+
+USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRBT
+USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
+USE MPL_MODULE  ,ONLY : MPL_ALLTOALLV, MPL_BARRIER, MPL_ALL_MS_COMM, MPL_MYRANK
+USE TPM_DISTR       ,ONLY : D, NPRTRW, NPROC, MYPROC, MYSETW
+USE TPM_GEN         ,ONLY : LSYNC_TRANS
+USE MPI
+
+IMPLICIT NONE
+
+INTEGER(KIND=JPIM) ,INTENT(IN)  :: KF_FS
+REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF(:)
+REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF_IN(:)
+
+INTEGER(KIND=JPIM) :: ILENS(NPRTRW),IOFFS(NPRTRW),ILENR(NPRTRW),IOFFR(NPRTRW)
+INTEGER(KIND=JPIM) :: J, ILEN, ISTA, FROM_SEND, TO_SEND, FROM_RECV, TO_RECV, IRANK
 REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
-REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR
-REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR2
-  
-  REAL(KIND=JPRBT)    :: ZDUM(1)
-  INTEGER(KIND=JPIM) :: IREQ
-  INTEGER(KIND=JPIM) :: IERROR
-  !     ------------------------------------------------------------------
-  
-  REAL(KIND=JPRBT) :: T1, T2, TIMEF, Tc
-  INTEGER(KIND=JPIM) :: MTOL_OR_LTOM, NOFULLPEERACCESS
-  INTEGER(KIND=JPIM) :: IRANK,IUNIT
-  INTEGER(KIND=JPIM) :: FROM_SEND,FROM_RECV,TO_RECV,TO_SEND
-
-
-  IF (LHOOK) CALL DR_HOOK('TRLTOM_CUDAAWARE',0,ZHOOK_HANDLE)
+INTEGER(KIND=JPIM) :: IERROR
 
 #ifdef PARKINDTRANS_SINGLE
 #define TRLTOM_DTYPE MPI_REAL
 #else
 #define TRLTOM_DTYPE MPI_DOUBLE_PRECISION
 #endif
-  
-  ITAG = MTAGLM
-  
-  IF(NPROC > 1) THEN
-    DO J=1,NPRTRW
-      ILENS(J) = D%NLTSGTB(J)*2*KF_FS
-      IOFFS(J) = D%NSTAGT0B(J)*2*KF_FS
-      ILENR(J) = D%NLTSFTB(J)*2*KF_FS
-      IOFFR(J) = D%NSTAGT1B(J)*2*KF_FS
-    ENDDO
 
-    CALL GSTATS(806,0)
+IF (LHOOK) CALL DR_HOOK('TRLTOM_CUDAAWARE',0,ZHOOK_HANDLE)
 
-    ! copy to self workaround
-    IRANK = MPL_MYRANK(MPL_ALL_MS_COMM)
-    IF (ILENS(IRANK) .ne. ILENR(IRANK)) THEN
-        PRINT *, "ERROR", ILENS(IRANK), ILENR(IRANK)
-        stop 1
-    ENDIF
-    IF (ILENS(IRANK) > 0) THEN
-        FROM_SEND = IOFFS(IRANK) + 1
-        TO_SEND = FROM_SEND + ILENS(IRANK) - 1
-        FROM_RECV = IOFFR(IRANK) + 1
-        TO_RECV = FROM_RECV + ILENR(IRANK) - 1
-        !$ACC KERNELS ASYNC(1) DEFAULT(NONE) PRESENT(PFBUF,PFBUF_IN)
-        PFBUF(FROM_RECV:TO_RECV) = PFBUF_IN(FROM_SEND:TO_SEND)
-        !$ACC END KERNELS
-        ILENS(IRANK) = 0
-        ILENR(IRANK) = 0
-    ENDIF
-  
-    !$ACC HOST_DATA USE_DEVICE(PFBUF_IN, PFBUF)
+IF(NPROC > 1) THEN
+  DO J=1,NPRTRW
+    ILENS(J) = D%NLTSGTB(J)*2*KF_FS
+    IOFFS(J) = D%NSTAGT0B(J)*2*KF_FS
+    ILENR(J) = D%NLTSFTB(J)*2*KF_FS
+    IOFFR(J) = D%NSTAGT1B(J)*2*KF_FS
+  ENDDO
 
-    IF (LSYNC_TRANS) THEN
-      CALL GSTATS(420,0)
-      CALL MPL_BARRIER(CDSTRING='TRLTOM BARRIER')
-      CALL GSTATS(420,1)
-    ENDIF
-    CALL GSTATS(411,0)
-    CALL MPI_ALLTOALLV(PFBUF_IN,ILENS,IOFFS,TRLTOM_DTYPE,&
-     & PFBUF,ILENR,IOFFR, TRLTOM_DTYPE, &
-     & MPL_ALL_MS_COMM, IERROR)
+  CALL GSTATS(806,0)
 
-    !$ACC END HOST_DATA
-    !$ACC WAIT(1)
+  ! copy to self workaround
+  IRANK = MPL_MYRANK(MPL_ALL_MS_COMM)
+  IF (ILENS(IRANK) .ne. ILENR(IRANK)) THEN
+      PRINT *, "ERROR", ILENS(IRANK), ILENR(IRANK)
+      stop 1
+  ENDIF
+  IF (ILENS(IRANK) > 0) THEN
+      FROM_SEND = IOFFS(IRANK) + 1
+      TO_SEND = FROM_SEND + ILENS(IRANK) - 1
+      FROM_RECV = IOFFR(IRANK) + 1
+      TO_RECV = FROM_RECV + ILENR(IRANK) - 1
+      !$ACC KERNELS ASYNC(1) DEFAULT(NONE) PRESENT(PFBUF,PFBUF_IN)
+      PFBUF(FROM_RECV:TO_RECV) = PFBUF_IN(FROM_SEND:TO_SEND)
+      !$ACC END KERNELS
+      ILENS(IRANK) = 0
+      ILENR(IRANK) = 0
+  ENDIF
 
+  !$ACC HOST_DATA USE_DEVICE(PFBUF_IN, PFBUF)
+
+  IF (LSYNC_TRANS) THEN
+    CALL GSTATS(420,0)
+    CALL MPL_BARRIER(CDSTRING='TRLTOM BARRIER')
+    CALL GSTATS(420,1)
+  ENDIF
+  CALL GSTATS(411,0)
+  CALL MPI_ALLTOALLV(PFBUF_IN,ILENS,IOFFS,TRLTOM_DTYPE,&
+   & PFBUF,ILENR,IOFFR, TRLTOM_DTYPE, &
+   & MPL_ALL_MS_COMM,IERROR)
+
+  !$ACC END HOST_DATA
+  !$ACC WAIT(1)
+
+  IF (LSYNC_TRANS) THEN
     CALL MPI_BARRIER(MPI_COMM_WORLD,IERROR)
-    CALL GSTATS(411,1)
-    CALL GSTATS(806,1)
-  ELSE
-    ILEN = D%NLTSGTB(MYSETW)*2*KF_FS
-    ISTA = D%NSTAGT1B(MYSETW)*2*KF_FS+1
-    CALL GSTATS(1607,0)
-    !$ACC PARALLEL LOOP DEFAULT(NONE) PRESENT(PFBUF,PFBUF_IN)
-    DO J=ISTA,ISTA+ILEN-1
-      PFBUF(J) = PFBUF_IN(J)
-    ENDDO
-    CALL GSTATS(1607,1)
   ENDIF
-  
-  IF (LHOOK) CALL DR_HOOK('TRLTOM_CUDAAWARE',1,ZHOOK_HANDLE)
-  !     ------------------------------------------------------------------
-  END SUBROUTINE TRLTOM_CUDAAWARE
-
-  SUBROUTINE TRLTOM(PFBUF_IN,PFBUF,KF_FS)
-  
-  !**** *TRLTOM * - transposition in Fourierspace
-  
-  !     Purpose.
-  !     --------
-  !              Transpose Fourier coefficients from partitioning
-  !              over latitudes to partitioning over wave numbers
-  !              This is done between inverse Legendre Transform
-  !              and inverse FFT.
-  !              This is the inverse routine of TRMTOL.
-  
-  !**   Interface.
-  !     ----------
-  !        *CALL* *TRLTOM(...)*
-  
-  !        Explicit arguments : PFBUF  - Fourier coefficient buffer. It is
-  !        --------------------          used for both input and output.
-  
-  !                             KF_FS - Number of fields communicated
-  
-  !        Implicit arguments :
-  !        --------------------
-  
-  !     Method.
-  !     -------
-  !        See documentation
-  
-  !     Externals.
-  !     ----------
-  
-  !     Reference.
-  !     ----------
-  !        ECMWF Research Department documentation of the IFS
-  
-  !     Author.
-  !     -------
-  !        MPP Group *ECMWF*
-  
-  !     Modifications.
-  !     --------------
-  !        Original : 95-10-01
-  !        Modified : 97-06-18 G. Mozdzynski - control MPI mailbox use
-  !                                            (NCOMBFLEN) for nphase.eq.1
-  !        Modified : 99-05-28  D.Salmond - Optimise copies.
-  !        Modified : 00-02-02  M.Hamrud  - Remove NPHASE
-  !        D.Salmond : 01-11-23 LIMP_NOOLAP Option for non-overlapping message
-  !                             passing and buffer packing
-  !        G.Mozdzynski : 08-01-01 Cleanup
-  !        Y.Seity   : 07-08-30 Add barrier synchonisation under LSYNC_TRANS
-  !     ------------------------------------------------------------------
-  
-  USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRBT
-  USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
-  
-  USE MPL_MODULE  ,ONLY : MPL_ALLTOALLV, MPL_BARRIER, MPL_ALL_MS_COMM, MPL_MYRANK, MPL_WAIT, JP_NON_BLOCKING_STANDARD
-  
-  USE TPM_DISTR       ,ONLY : D, MTAGLM, MYSETW, NPRTRW, NPROC, MYPROC
-  USE TPM_GEN         ,ONLY : LSYNC_TRANS
-  
-  USE MPI
-  
-  !USE SET2PE_MOD
-  !USE MYSENDSET_MOD
-  !USE MYRECVSET_MOD
-  !USE ABORT_TRANS_MOD
-  !
-  
-  IMPLICIT NONE
-  
-  INTEGER(KIND=JPIM),INTENT(IN)  :: KF_FS
-  REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF(:)
-  REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF_IN(:)
-  
-  INTEGER(KIND=JPIM) :: ILENS(NPRTRW),IOFFS(NPRTRW),ILENR(NPRTRW),IOFFR(NPRTRW)
-  
-  INTEGER(KIND=JPIM) :: ITAG, J, ILEN, ISTA
-  
-REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
-REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR
-REAL(KIND=JPHOOK) :: ZHOOK_HANDLE_BAR2
-  
-  REAL(KIND=JPRBT)    :: ZDUM(1)
-  INTEGER(KIND=JPIM) :: IREQ
-  INTEGER(KIND=JPIM) :: IERROR
-  !     ------------------------------------------------------------------
-  
-  REAL(KIND=JPRBT) :: T1, T2, TIMEF, tc
-  INTEGER(KIND=JPIM) :: MTOL_OR_LTOM, NOFULLPEERACCESS
-  INTEGER(KIND=JPIM) :: IRANK,iunit
-
-  IF (LHOOK) CALL DR_HOOK('TRLTOM',0,ZHOOK_HANDLE)
-  
-  ITAG = MTAGLM
-  
-  IF(NPROC > 1) THEN
-    DO J=1,NPRTRW
-      ILENS(J) = D%NLTSGTB(J)*2*KF_FS
-      IOFFS(J) = D%NSTAGT0B(J)*2*KF_FS
-      ILENR(J) = D%NLTSFTB(J)*2*KF_FS
-      IOFFR(J) = D%NSTAGT1B(J)*2*KF_FS
-    ENDDO
-
-    CALL GSTATS(806,0)
-    !$ACC UPDATE HOST(PFBUF_IN)
-  
-    CALL MPL_ALLTOALLV(PSENDBUF=PFBUF_IN,KSENDCOUNTS=ILENS,&
-     & PRECVBUF=PFBUF,KRECVCOUNTS=ILENR,KSENDDISPL=IOFFS,KRECVDISPL=IOFFR,&
-     & KCOMM=MPL_ALL_MS_COMM,CDSTRING='TRLTOM:')
-
-    call MPI_BARRIER(MPI_COMM_WORLD,IERROR)
-    !$ACC UPDATE DEVICE(PFBUF)
+  CALL GSTATS(411,1)
   CALL GSTATS(806,1)
-  ELSE
-    ILEN = D%NLTSGTB(MYSETW)*2*KF_FS
-    ISTA = D%NSTAGT1B(MYSETW)*2*KF_FS+1
-    CALL GSTATS(1607,0)
-    DO J=ISTA,ISTA+ILEN-1
-      PFBUF(J) = PFBUF_IN(J)
-    ENDDO
-    CALL GSTATS(1607,1)
-  ENDIF
-  
-  IF (LHOOK) CALL DR_HOOK('TRLTOM',1,ZHOOK_HANDLE)
-  !     ------------------------------------------------------------------
-  END SUBROUTINE TRLTOM
-  END MODULE TRLTOM_MOD
+ELSE
+  ILEN = D%NLTSGTB(MYSETW)*2*KF_FS
+  ISTA = D%NSTAGT1B(MYSETW)*2*KF_FS+1
+  CALL GSTATS(1607,0)
+  !$ACC PARALLEL LOOP DEFAULT(NONE) PRESENT(PFBUF,PFBUF_IN)
+  DO J=ISTA,ISTA+ILEN-1
+    PFBUF(J) = PFBUF_IN(J)
+  ENDDO
+  CALL GSTATS(1607,1)
+ENDIF
+
+IF (LHOOK) CALL DR_HOOK('TRLTOM_CUDAAWARE',1,ZHOOK_HANDLE)
+!     ------------------------------------------------------------------
+END SUBROUTINE TRLTOM_CUDAAWARE
+
+SUBROUTINE TRLTOM(PFBUF_IN,PFBUF,KF_FS)
+
+!**** *TRLTOM * - transposition in Fourier space
+
+!     Purpose.
+!     --------
+!              Transpose Fourier coefficients from partitioning
+!              over latitudes to partitioning over wave numbers
+!              This is done between inverse Legendre Transform
+!              and inverse FFT.
+!              This is the inverse routine of TRMTOL.
+
+!**   Interface.
+!     ----------
+!        *CALL* *TRLTOM(...)*
+
+!        Explicit arguments : PFBUF  - Fourier coefficient buffer. It is
+!        --------------------          used for both input and output.
+!                             KF_FS - Number of fields communicated
+
+!        Implicit arguments :
+!        --------------------
+
+!     Method.
+!     -------
+!        See documentation
+
+!     Externals.
+!     ----------
+
+!     Reference.
+!     ----------
+!        ECMWF Research Department documentation of the IFS
+
+!     Author.
+!     -------
+!        MPP Group *ECMWF*
+
+!     Modifications.
+!     --------------
+!        Original : 95-10-01
+!        Modified : 97-06-18 G. Mozdzynski - control MPI mailbox use
+!                                            (NCOMBFLEN) for nphase.eq.1
+!        Modified : 99-05-28  D.Salmond - Optimise copies.
+!        Modified : 00-02-02  M.Hamrud  - Remove NPHASE
+!        D.Salmond : 01-11-23 LIMP_NOOLAP Option for non-overlapping message
+!                             passing and buffer packing
+!        G.Mozdzynski : 08-01-01 Cleanup
+!        Y.Seity   : 07-08-30 Add barrier synchronisation under LSYNC_TRANS
+!     ------------------------------------------------------------------
+
+USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRBT
+USE YOMHOOK   ,ONLY : LHOOK,   DR_HOOK, JPHOOK
+USE MPL_MODULE  ,ONLY : MPL_ALLTOALLV, MPL_BARRIER, MPL_ALL_MS_COMM
+USE TPM_DISTR       ,ONLY : D, NPRTRW, NPROC, MYPROC, MYSETW
+USE TPM_GEN         ,ONLY : LSYNC_TRANS
+USE MPI
+
+IMPLICIT NONE
+
+INTEGER(KIND=JPIM) ,INTENT(IN)  :: KF_FS
+REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF(:)
+REAL(KIND=JPRBT)   ,INTENT(INOUT)  :: PFBUF_IN(:)
+
+INTEGER(KIND=JPIM) :: ILENS(NPRTRW),IOFFS(NPRTRW),ILENR(NPRTRW),IOFFR(NPRTRW)
+INTEGER(KIND=JPIM) :: J, ILEN, ISTA
+REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
+INTEGER(KIND=JPIM) :: IERROR
+
+IF (LHOOK) CALL DR_HOOK('TRLTOM',0,ZHOOK_HANDLE)
+
+IF(NPROC > 1) THEN
+  DO J=1,NPRTRW
+    ILENS(J) = D%NLTSGTB(J)*2*KF_FS
+    IOFFS(J) = D%NSTAGT0B(J)*2*KF_FS
+    ILENR(J) = D%NLTSFTB(J)*2*KF_FS
+    IOFFR(J) = D%NSTAGT1B(J)*2*KF_FS
+  ENDDO
+
+  CALL GSTATS(806,0)
+  !$ACC UPDATE HOST(PFBUF_IN)
+
+  CALL MPL_ALLTOALLV(PSENDBUF=PFBUF_IN,KSENDCOUNTS=ILENS,&
+   & PRECVBUF=PFBUF,KRECVCOUNTS=ILENR,KSENDDISPL=IOFFS,KRECVDISPL=IOFFR,&
+   & KCOMM=MPL_ALL_MS_COMM,CDSTRING='TRLTOM:')
+
+  !$ACC UPDATE DEVICE(PFBUF)
+  CALL MPI_BARRIER(MPI_COMM_WORLD,IERROR)
+  CALL GSTATS(806,1)
+ELSE
+  ILEN = D%NLTSGTB(MYSETW)*2*KF_FS
+  ISTA = D%NSTAGT1B(MYSETW)*2*KF_FS+1
+  CALL GSTATS(1607,0)
+  !$ACC PARALLEL LOOP DEFAULT(NONE) PRESENT(PFBUF,PFBUF_IN)
+  DO J=ISTA,ISTA+ILEN-1
+    PFBUF(J) = PFBUF_IN(J)
+  ENDDO
+  CALL GSTATS(1607,1)
+ENDIF
+
+IF (LHOOK) CALL DR_HOOK('TRLTOM',1,ZHOOK_HANDLE)
+!     ------------------------------------------------------------------
+END SUBROUTINE TRLTOM
+END MODULE TRLTOM_MOD
