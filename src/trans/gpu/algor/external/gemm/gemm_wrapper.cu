ba65812dab1232d54f5bad2203ff34d0617613fa
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:49:27 -0700

    Add grouped GEMM in LEINV

--- src/trans/gpu/algor/external/gemm/gemm_wrapper.cu
+++ src/trans/gpu/algor/external/gemm/gemm_wrapper.cu
@@ -48,4 +48,37 @@ void cublas_sgemm_wrapper(cublasOperation_t transa, cublasOperation_t transb,
                                          &alpha, A, lda, tda, B, ldb, tdb,
                                          &beta, C, ldc, tdc, batchCount));
 }
+
+void cublas_sgemm_wrapper_grouped(cublasOperation_t transa,
+                                  cublasOperation_t transb, int m, int *n,
+                                  int *k, float alpha, const float *A, int lda,
+                                  int tda, const float *B, int ldb, int tdb,
+                                  float beta, float *C, int ldc, int tdc,
+                                  int batchCount) {
+  static cublasHandle_t handle = nullptr;
+  if (!handle)
+    CUBLAS_CHECK(cublasCreate(&handle));
+
+  for (int i = 0; i < batchCount; ++i) {
+    CUBLAS_CHECK(cublasSgemm(handle, transa, transb, m, n[i], k[i], &alpha,
+                             A + i * tda, lda, B + i * tdb, ldb, &beta,
+                             C + i * tdc, ldc));
+  }
+}
+void cublas_dgemm_wrapper_grouped(cublasOperation_t transa,
+                                  cublasOperation_t transb, int m, int *n,
+                                  int *k, double alpha, const double *A,
+                                  int lda, int tda, const double *B, int ldb,
+                                  int tdb, double beta, double *C, int ldc,
+                                  int tdc, int batchCount) {
+  static cublasHandle_t handle = nullptr;
+  if (!handle)
+    CUBLAS_CHECK(cublasCreate(&handle));
+
+  for (int i = 0; i < batchCount; ++i) {
+    CUBLAS_CHECK(cublasDgemm(handle, transa, transb, m, n[i], k[i], &alpha,
+                             A + i * tda, lda, B + i * tdb, ldb, &beta,
+                             C + i * tdc, ldc));
+  }
+}
 }
