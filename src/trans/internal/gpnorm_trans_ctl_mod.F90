dae7c0da9c7e3367ef2b744b455105cf94589b74
Olivier Marsden <olivier.marsden@ecmwf.int>
2022-06-08 15:34:34 +0000

    Harmonise dates across relevant ECMWF and Meteo-France copyright statements.

--- src/trans/internal/gpnorm_trans_ctl_mod.F90
+++ src/trans/internal/gpnorm_trans_ctl_mod.F90
@@ -1,5 +1,5 @@
 ! (C) Copyright 2008- ECMWF.
-! (C) Copyright 2013- Meteo-France.
+! (C) Copyright 2008- Meteo-France.
 ! 
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
