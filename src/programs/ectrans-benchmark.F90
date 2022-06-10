0faaaaecadcb58c64f736c0df0775da8ef77ea49
Willem Deconinck <willem.deconinck@ecmwf.int>
2022-06-10 17:43:42 +0100

    Fix ectrans-benchmark with MPI: dr_hook_init needs to be after mpl_init

--- src/programs/ectrans-benchmark.F90
+++ src/programs/ectrans-benchmark.F90
@@ -206,7 +206,7 @@ integer(kind=jpim) :: jend_vder_EW = 0
 
 logical :: ldump_values = .false.
 
-integer, external :: ec_mpisize, ec_mpirank
+integer, external :: ec_mpirank
 logical :: luse_mpi = .true.
 
 character(len=16) :: cgrid
@@ -224,9 +224,7 @@ character(len=16) :: cgrid
 
 !===================================================================================================
 
-if (ec_mpisize() == 1 ) then
-  luse_mpi = .false.
-endif
+luse_mpi = detect_mpirun()
 
 ! Setup
 call get_command_line_arguments(nsmax, cgrid, iters, nfld, nlev, lvordiv, lscders, luvders, luseflt, nproma, verbose, ldump_values)
@@ -236,7 +234,6 @@ nflevg = nfld
 
 !===================================================================================================
 
-call dr_hook_init()
 if (luse_mpi) then
   call mpl_init()
   nproc  = mpl_nproc()
@@ -246,6 +243,8 @@ else
   myproc = 1
 endif
 nthread = oml_max_threads()
+
+call dr_hook_init()
 
 !===================================================================================================
 
@@ -1243,6 +1242,31 @@ write(noutdump) reshape(fld, (/ nproma*ngpblks /))
 close(noutdump)
 
 end subroutine dump_gridpoint_field
+
+function detect_mpirun() result(lmpi_required)
+  logical :: lmpi_required
+  integer :: ilen
+  integer, parameter :: nvars = 5
+  character(len=32), dimension(nvars) :: cmpirun_detect
+  character(len=4) :: clenv_dr_hook_assert_mpi_initialized
+  integer :: ivar
+
+  ! Environment variables that are set when mpirun, srun, aprun, ... are used
+  cmpirun_detect(1) = 'OMPI_COMM_WORLD_SIZE'  ! openmpi
+  cmpirun_detect(2) = 'ALPS_APP_PE'           ! cray pe
+  cmpirun_detect(3) = 'PMI_SIZE'              ! intel
+  cmpirun_detect(4) = 'SLURM_NTASKS'          ! slurm
+  cmpirun_detect(5) = 'ECTRANS_USE_MPI'       ! forced
+
+  lmpi_required = .false.
+  do ivar=1,nvars
+    call get_environment_variable(name=trim(cmpirun_detect(ivar)),length=ilen)
+    if (ilen > 0) then
+      lmpi_required = .true.
+      exit ! break
+    endif
+  enddo
+end function
 
 end program transform_test
 
