1b11f84b7e06442ded4e25e486ddbd36e7685423
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2023-11-13 12:07:17 +0100

    Revert to CMAKE_C_COMPILER in preprocessing invocation to address CLANG existence under gcc, split ectrans module from submodules for more flexible compilation

--- src/trans/module/ectrans_mod.F90
+++ src/trans/module/ectrans_mod.F90
@@ -9,28 +9,3 @@ module ectrans_mod
 #include "specnorm.h"
 contains
 end module ectrans_mod
-submodule (ectrans_mod) setup_trans0_mod
-contains
-#include "setup_trans0.F90"
-end submodule setup_trans0_mod
-submodule (ectrans_mod) setup_trans_mod
-contains
-#include "setup_trans.F90"
-end submodule setup_trans_mod
-submodule (ectrans_mod) inv_trans_mod
-contains
-#include "inv_trans.F90"
-end submodule inv_trans_mod
-submodule (ectrans_mod) dir_trans_mod
-contains
-#include "dir_trans.F90"
-end submodule dir_trans_mod
-submodule (ectrans_mod) trans_inq__mod
-contains
-#include "trans_inq.F90"
-end submodule trans_inq__mod
-submodule (ectrans_mod) specnorm_mod
-contains
-#include "specnorm.F90"
-end submodule specnorm_mod
-
