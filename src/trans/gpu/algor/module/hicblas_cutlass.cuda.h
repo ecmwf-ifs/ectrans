ffdc2750bf54008be78dbdf402b6133af842f7cf
Olivier Marsden <Olivier.Marsden@ecmwf.int>
2023-12-06 21:56:26 +0000

    Adapt 3xTF32 for hicblas interface, make compile-time optional.
    * NB : compile time option should maybe check on minimal cuda arch of sm80, but
           can be done later.

--- src/trans/gpu/algor/module/hicblas_cutlass.cuda.h
+++ src/trans/gpu/algor/module/hicblas_cutlass.cuda.h
@@ -12,7 +12,11 @@
     }                                                                          \
   }
 
-
+#ifdef USE_3XTF32
+constexpr bool use_3xtf32 = true;
+#else
+constexpr bool use_3xtf32 = false;
+#endif
 
 
 template <typename CutlassGemm>
@@ -20,10 +24,71 @@ CutlassGemm &get_cutlass_handle() {
   static auto handle = std::make_unique<CutlassGemm>();
   return *handle;
 }
- 
+
 namespace detail {
+
+enum class CutlassType { cutlass_3xtf32, cutlass_fp32 };
+
+template <CutlassType, cublasOperation_t TransA, cublasOperation_t TransB>
+class cutlass_sgemm_grouped;
+
 template <cublasOperation_t TransA, cublasOperation_t TransB>
-class cutlass_sgemm_grouped {
+class cutlass_sgemm_grouped<CutlassType::cutlass_3xtf32, TransA, TransB> {
+  // this was verified using Ampere and uses 3XTF32
+  static constexpr int AlignmentA = 4;
+  static constexpr int AlignmentB = 4;
+  using ThreadblockShape = cutlass::gemm::GemmShape<128, 64, 32>;
+  using WarpShape = cutlass::gemm::GemmShape<64, 32, 32>;
+  using InstructionShape = cutlass::gemm::GemmShape<16, 8, 8>;
+  using OperatorClass = cutlass::arch::OpClassTensorOp;
+  using MyOp = cutlass::arch::OpMultiplyAddFastF32;
+
+  using Gemm = cutlass::gemm::device::Gemm<
+      float,
+      std::conditional_t<TransA == CUBLAS_OP_N, cutlass::layout::ColumnMajor,
+                         cutlass::layout::RowMajor>,  //
+      float,
+      std::conditional_t<TransB == CUBLAS_OP_N, cutlass::layout::ColumnMajor,
+                         cutlass::layout::RowMajor>,  //
+      float, cutlass::layout::ColumnMajor,            //
+      float,                                          //
+      OperatorClass, cutlass::arch::Sm80,             //
+      ThreadblockShape, WarpShape, InstructionShape,  //
+      cutlass::epilogue::thread::LinearCombination<   //
+          float,                                      //
+          128 / cutlass::sizeof_bits<float>::value,
+          float,                                                     //
+          float                                                      //
+          >,                                                         //
+      cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<>,  //
+      3,                                                             //
+      AlignmentA,                                                    //
+      AlignmentB,                                                    //
+      true,                                                          //
+      MyOp                                                           //
+      >;
+  static constexpr int sz_align = 8;
+
+ public:
+  void operator()(cudaStream_t stream, int m, int n, int k, float alpha,
+                  const float *A, int lda, const float *B, int ldb, float beta,
+                  float *C, int ldc) const {
+    auto &gemm_op = get_cutlass_handle<Gemm>();
+    CUTLASS_CHECK(gemm_op(
+        {//
+         {(m + sz_align - 1) / sz_align * sz_align,
+          (n + sz_align - 1) / sz_align * sz_align,
+          (k + sz_align - 1) / sz_align * sz_align},
+         {const_cast<float *>(A), lda},
+         {const_cast<float *>(B), ldb},
+         {C, ldc},
+         {C, ldc},
+         {alpha, beta}},
+        nullptr, stream));
+  }
+};
+template <cublasOperation_t TransA, cublasOperation_t TransB>
+class cutlass_sgemm_grouped<CutlassType::cutlass_fp32, TransA, TransB> {
   // this was verified using Volta and uses FP32
   static constexpr int AlignmentA = 1;
   static constexpr int AlignmentB = 1;
@@ -42,7 +107,7 @@ class cutlass_sgemm_grouped {
                          cutlass::layout::RowMajor>,                //
       float, cutlass::layout::ColumnMajor,                          //
       float,                                                        //
-      OperatorClass, cutlass::arch::Sm50,                           //
+      OperatorClass, cutlass::arch::Sm70,                           //
       ThreadblockShape, WarpShape, InstructionShape,                //
       cutlass::epilogue::thread::LinearCombination<                 //
           float,                                                    //
@@ -88,9 +153,21 @@ void cutlass_sgemm_wrapper_grouped_op(int blas_id, int m, int *n, int *k,
                                       int ldc, int *offsetsC, int batchCount,
 				      cudaStream_t stream) {
   using namespace detail;
-  run_group_graph(cutlass_sgemm_grouped<TransA, TransB>(), m, n, k, alpha, A,
-                  lda, offsetsA, B, ldb, offsetsB, beta, C, ldc, offsetsC,
-                  batchCount, stream, blas_id);
+  int device;
+  HIC_CHECK(cudaGetDevice(&device));
+  int capability_major;
+  HIC_CHECK(cudaDeviceGetAttribute(&capability_major,
+                                    cudaDevAttrComputeCapabilityMajor, device));
+  if (capability_major >= 8 && use_3xtf32)
+    run_group_graph(cutlass_sgemm_grouped<detail::CutlassType::cutlass_3xtf32,
+                                          TransA, TransB>(),
+                    m, n, k, alpha, A, lda, offsetsA, B, ldb, offsetsB, beta, C,
+                    ldc, offsetsC, batchCount, stream, blas_id);
+  else
+    run_group_graph(cutlass_sgemm_grouped<detail::CutlassType::cutlass_fp32,
+                                          TransA, TransB>(),
+                    m, n, k, alpha, A, lda, offsetsA, B, ldb, offsetsB, beta, C,
+                    ldc, offsetsC, batchCount, stream, blas_id);
 }
 
 void cutlass_sgemm_wrapper_grouped(int blas_id, char transa, char transb,
