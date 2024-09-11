32c6f9939aa2db2a8f7c5b4fa338c64ce97f235f
Sam Hatfield <samuel.hatfield@ecmwf.int>
2024-09-11 08:28:07 +0000

    Add back comment separator

--- src/programs/ectrans-benchmark.F90
+++ src/programs/ectrans-benchmark.F90
@@ -1315,6 +1315,8 @@ subroutine initialize_spectral_field(nsmax, field)
 
 end subroutine initialize_spectral_field
 
+!===================================================================================================
+
 subroutine initialize_2d_spectral_field(nsmax, field)
 
   integer,         intent(in)    :: nsmax    ! Spectral truncation
