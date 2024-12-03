2c5e13021ffda711882d8dfd2125620b0a5070a2
Daan Degrauwe <daan.degrauwe@meteo.be>
2024-12-03 13:58:33 +0000

    added license information to etrans sources

--- src/etrans/cpu/external/esetup_trans.F90
+++ src/etrans/cpu/external/esetup_trans.F90
@@ -1,3 +1,14 @@
+! (C) Copyright 2001- ECMWF.
+! (C) Copyright 2001- Meteo-France.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+! 
+
+
 SUBROUTINE ESETUP_TRANS(KMSMAX,KSMAX,KDGL,KDGUX,KLOEN,LDSPLIT,&
  & KTMAX,KRESOL,PEXWN,PEYWN,PWEIGHT,LDGRIDONLY,KNOEXTZL,KNOEXTZG, &
  & LDUSEFFTW,LD_ALL_FFTW)
--- src/etrans/cpu/internal/eleinv_mod.F90
+++ src/etrans/cpu/internal/eleinv_mod.F90
@@ -1,3 +1,14 @@
+! (C) Copyright 2001- ECMWF.
+! (C) Copyright 2001- Meteo-France.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+! 
+
+
 MODULE ELEINV_MOD
 CONTAINS
 SUBROUTINE ELEINV(KM,KFC,KF_OUT_LT,PIA)
