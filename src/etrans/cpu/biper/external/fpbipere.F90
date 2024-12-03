c3a21dca162b97cb9872a89183fb123dc59b2cb6
Daan Degrauwe <daan.degrauwe@meteo.be>
2024-12-03 13:58:33 +0000

    added license information to etrans sources

--- src/etrans/cpu/biper/external/fpbipere.F90
+++ src/etrans/cpu/biper/external/fpbipere.F90
@@ -1,3 +1,14 @@
+! (C) Copyright 2001- ECMWF.
+! (C) Copyright 2001- Meteo-France.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+! 
+
+
 SUBROUTINE FPBIPERE(KDLUX,KDGUX,KDLON,KDGL,KNUBI,KD1,PGPBI,KDADD,LDZON, &
  & LDBOYD, KDBOYD, PLBOYD)
 
