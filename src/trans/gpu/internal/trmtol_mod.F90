9eea9083d0213091df4ee83f798f9817e23da559
Sam Hatfield <samuel.hatfield@ecmwf.int>
2024-12-11 11:28:51 +0200

    Refresh OpenMP in TRMTOL

--- src/trans/gpu/internal/trmtol_mod.F90
+++ src/trans/gpu/internal/trmtol_mod.F90
@@ -160,6 +160,11 @@ CONTAINS
           FROM_RECV = IOFFR(IRANK) + 1
           TO_RECV = FROM_RECV + ILENR(IRANK) - 1
 #ifdef OMPGPU
+          !$OMP TARGET TEAMS DEFAULT(NONE) &
+          !$OMP& SHARED(PFBUF,PFBUF_IN,FROM_RECV,TO_RECV,FROM_SEND,TO_SEND) &
+          !$OMP& MAP(TO:FROM_RECV,TO_RECV,FROM_SEND,TO_SEND)
+          !$OMP PARALLEL
+          !$OMP WORKSHARE
 #endif
 #ifdef ACCGPU
 #ifdef __HIP_PLATFORM_AMD__
@@ -171,6 +176,9 @@ CONTAINS
 #endif
           PFBUF(FROM_RECV:TO_RECV) = PFBUF_IN(FROM_SEND:TO_SEND)
 #ifdef OMPGPU
+          !$OMP END WORKSHARE
+          !$OMP END PARALLEL
+          !$OMP END TARGET TEAMS
 #endif
 #ifdef ACCGPU
           !$ACC END KERNELS
@@ -187,13 +195,19 @@ CONTAINS
       CALL GSTATS(421,0)
 #ifdef USE_GPU_AWARE_MPI
 #ifdef OMPGPU
+      !$OMP TARGET DATA USE_DEVICE_PTR(PFBUF_IN,PFBUF)
 #endif
 #ifdef ACCGPU
       !$ACC HOST_DATA USE_DEVICE(PFBUF_IN, PFBUF)
 #endif
 #else
-    !! this is safe-but-slow fallback for running without GPU-aware MPI
-    !$ACC UPDATE HOST(PFBUF_IN,PFBUF)
+      !! this is safe-but-slow fallback for running without GPU-aware MPI
+#ifdef OMPGPU
+      !$OMP TARGET UPDATE FROM(PFBUF_IN,PFBUF)
+#endif
+#ifdef ACCGPU
+      !$ACC UPDATE HOST(PFBUF_IN,PFBUF)
+#endif
 #endif
 
 #if ECTRANS_HAVE_MPI
@@ -205,14 +219,20 @@ CONTAINS
 #endif
 
 #ifdef USE_GPU_AWARE_MPI
-#ifdef OMPGPU
-#endif
 #ifdef ACCGPU
       !$ACC END HOST_DATA
 #endif
+#ifdef OMPGPU
+      !$OMP END TARGET DATA
+#endif
 #else
-    !! this is safe-but-slow fallback for running without GPU-aware MPI
-    !$ACC UPDATE DEVICE(PFBUF)
+      !! this is safe-but-slow fallback for running without GPU-aware MPI
+#ifdef ACCGPU
+      !$ACC UPDATE DEVICE(PFBUF)
+#endif
+#ifdef OMPGPU
+      !$OMP TARGET UPDATE TO(PFBUF)
+#endif
 #endif
       IF (LSYNC_TRANS) THEN
         CALL GSTATS(441,0)
@@ -234,6 +254,8 @@ CONTAINS
       IEND = ISTA+ILEN-1
       CALL GSTATS(1608,0)
 #ifdef OMPGPU
+      !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO DEFAULT(NONE) &
+      !$OMP SHARED(PFBUF,PFBUF_IN,ISTA,IEND,ILEN) MAP(TO:ISTA,ILEN)
 #endif
 #ifdef ACCGPU
       !$ACC PARALLEL LOOP DEFAULT(NONE) PRESENT(PFBUF,PFBUF_IN) FIRSTPRIVATE(ISTA,IEND)
