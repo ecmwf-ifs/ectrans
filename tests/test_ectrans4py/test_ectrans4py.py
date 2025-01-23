08fbf233d263f7f89717266bc211993cfec504eb
Alexandre MARY <alexandre.mary@meteo.fr>
2025-01-23 16:31:24 +0100

    add sp2gp/gp2sp tests

--- tests/test_ectrans4py/test_ectrans4py.py
+++ tests/test_ectrans4py/test_ectrans4py.py
@@ -6,9 +6,20 @@ import ectrans4py
 ectrans4py.init_env()
 
 KNUMMAXRESOL = 10
+EPSILON = 1e-13
 
 
-class TestLAM(TestCase):
+class ArraysAlmostEqual(object):
+
+    def assert_arrays_diff_under_epsilon(self, x, y):
+        diff = x - y
+        diffmax = abs(diff.max())
+        diffmin = abs(diff.min())
+        self.assertTrue(diffmax < EPSILON, "diffmax is {}".format(diffmax))
+        self.assertTrue(diffmin < EPSILON, "diffmin is {}".format(diffmin))
+
+
+class TestLAM(TestCase, ArraysAlmostEqual):
 
     gpdims = {'X':54,
               'Y':48,
@@ -18,10 +29,12 @@ class TestLAM(TestCase):
               'Y_resolution':1300.0}
     truncation = {'in_X':26,
                   'in_Y':23}
-    spectra_data_sizes = (2592, 1968)
+    spectral_data_sizes = (2592, 1968)
+    spdata = data.antwrp1300['sp']
+    gpdata = data.antwrp1300['sp2gp']
 
     def test_etrans_inq(self):
-        spectra_data_sizes = ectrans4py.etrans_inq4py(
+        spectral_data_sizes = ectrans4py.etrans_inq4py(
             self.gpdims['X'],
             self.gpdims['Y'],
             self.gpdims['X_CIzone'],
@@ -31,10 +44,42 @@ class TestLAM(TestCase):
             KNUMMAXRESOL,
             self.gpdims['X_resolution'],
             self.gpdims['Y_resolution'])
-        self.assertEqual(spectra_data_sizes, self.spectra_data_sizes)
+        self.assertEqual(spectral_data_sizes, self.spectral_data_sizes)
 
+    def test_sp2gp(self):
+        gpdata = ectrans4py.sp2gp_lam4py(
+            self.gpdims['X'],
+            self.gpdims['Y'],
+            self.gpdims['X_CIzone'],
+            self.gpdims['Y_CIzone'],
+            self.truncation['in_X'],
+            self.truncation['in_Y'],
+            KNUMMAXRESOL,
+            len(self.spdata.flatten()),
+            False,  # no derivatives
+            False, # spectral_coeff_order != 'model',
+            self.gpdims['X_resolution'],
+            self.gpdims['Y_resolution'],
+            self.spdata.flatten())[0]
+        self.assert_arrays_diff_under_epsilon(gpdata, gpdata.flatten())
+    
+    def test_gp2sp(self):
+        spdata = ectrans4py.gp2sp_lam4py(
+            self.spectral_data_sizes[1],
+            self.gpdims['X'],
+            self.gpdims['Y'],
+            self.gpdims['X_CIzone'],
+            self.gpdims['Y_CIzone'],
+            self.truncation['in_X'],
+            self.truncation['in_Y'],
+            KNUMMAXRESOL,
+            self.gpdims['X_resolution'],
+            self.gpdims['Y_resolution'],
+            False,  # spectral_coeff_order != 'model',
+            self.gpdata.flatten())
+        self.assert_arrays_diff_under_epsilon(spdata, spdata.flatten())
 
-class TestGlobal(TestCase):
+class TestGlobal(TestCase, ArraysAlmostEqual):
 
     gpdims = {'lat_number':150,
               'lon_number_by_lat':data.lon_number_by_lat}
@@ -43,13 +88,43 @@ class TestGlobal(TestCase):
             33052,
             11175,
             data.zonal_wavenumbers)
+    spdata = data.tl149_c24['sp']
+    gpdata = data.tl149_c24['sp2gp']
 
     def test_trans_inq4py(self):
         spectral_data_sizes = ectrans4py.trans_inq4py(
             self.gpdims['lat_number'],
             self.truncation['max'],
             len(self.gpdims['lon_number_by_lat']),
-            numpy.array(self.gpdims['lon_number_by_lat']),
+            self.gpdims['lon_number_by_lat'],
             KNUMMAXRESOL)
         self.assertEqual(spectral_data_sizes[0:2], self.spectral_data_sizes[0:2])  # dimensions
         numpy.testing.assert_array_equal(spectral_data_sizes[2], self.spectral_data_sizes[2])  # zonal_wavenumbers
+    
+    def test_sp2gp(self):
+        gpdata = ectrans4py.sp2gp_gauss4py(
+            self.gpdims['lat_number'],
+            self.truncation['max'],
+            KNUMMAXRESOL,
+            sum(self.gpdims['lon_number_by_lat']),
+            len(self.gpdims['lon_number_by_lat']),
+            self.gpdims['lon_number_by_lat'],
+            len(self.spdata.flatten()),
+            False,  # no derivatives
+            False, # spectral_coeff_order != 'model',
+            self.spdata.flatten())[0]
+        self.assert_arrays_diff_under_epsilon(gpdata, gpdata.flatten())
+    
+    def test_gp2sp(self):
+        spdata = ectrans4py.gp2sp_gauss4py(
+            self.spectral_data_sizes[1] * 2,  # *2 for complex coefficients
+            self.gpdims['lat_number'],
+            self.truncation['max'],
+            KNUMMAXRESOL,
+            len(self.gpdims['lon_number_by_lat']),
+            self.gpdims['lon_number_by_lat'],
+            len(self.gpdata.flatten()),
+            False,  # spectral_coeff_order != 'model',
+            self.gpdata.flatten())
+        self.assert_arrays_diff_under_epsilon(spdata, spdata.flatten())
+
