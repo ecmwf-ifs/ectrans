b224c09fbc765ff1dac1c8eace44abcdab220f7f
Sam Hatfield <samuel.hatfield@ecmwf.int>
2025-02-20 16:02:49 +0000

    Increase adjoint test tolerance
    With the previous value, 100, the test failed in the Github CI suite for
    clang-12/Release and gnu-10/Debug.

--- tests/trans/test_adjoint.F90
+++ tests/trans/test_adjoint.F90
@@ -168,12 +168,12 @@ WRITE(NOUT, '(A,1E9.2)') '<Fx,y>  = ', ZSC1
 WRITE(NOUT, '(A,1E9.2)') '<x,F*y> = ', ZSC2
 WRITE(NOUT, '(A,1E9.2)') 'Relative error = ', ZRELATIVE_ERROR
 
-! Abort if relative error is > 100 * machine epsilon
-IF (ZRELATIVE_ERROR > 100.0*EPSILON(1.0_JPRB)) THEN
+! Abort if relative error is > 500 * machine epsilon
+IF (ZRELATIVE_ERROR > 500.0*EPSILON(1.0_JPRB)) THEN
   WRITE(NOUT, '(A)') '*******************************'
   WRITE(NOUT, '(A)') 'Adjoint test failed'
-  WRITE(NOUT, '(A)') 'Relative error greater than 100 * machine epsilon'
-  WRITE(NOUT, '(1E9.2,A3,1E9.2)') ZRELATIVE_ERROR, ' > ', 100.0*EPSILON(1.0_JPRB)
+  WRITE(NOUT, '(A)') 'Relative error greater than 500 * machine epsilon'
+  WRITE(NOUT, '(1E9.2,A3,1E9.2)') ZRELATIVE_ERROR, ' > ', 500.0*EPSILON(1.0_JPRB)
   WRITE(NOUT, '(A)') '*******************************'
   ERROR STOP
 ENDIF
