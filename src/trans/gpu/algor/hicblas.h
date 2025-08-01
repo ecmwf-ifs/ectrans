1ea72c5b55f2ff5bb86a493a64cd9a22408d6677
Sam Hatfield <samuel.hatfield@ecmwf.int>
2025-08-01 17:07:58 +0100

    Introduce skeleton for "host-side GPU emulation"

--- src/trans/gpu/algor/hicblas.h
+++ src/trans/gpu/algor/hicblas.h
@@ -22,6 +22,8 @@
 #include "hicblas_hip.h"
 #elif defined(CUDAGPU)
 #include "hicblas_cuda.h"
+#elif defined (HOSTGPU)
+#include "hicblas_host.h"
 #endif
 
 inline void _printError(const char * component, const char * file, const int line, int err, const char * err_str) {
