8f58b07d11a1235b8c9fa4801b283c5788f2e304
Willem Deconinck <willem.deconinck@ecmwf.int>
2025-08-18 11:44:53 +0000

    Revert "Fix for cublas link failures (#302)"
    This reverts commit d93d764017c95e8f252aa2925a6a900125be6943.
    
    The reverted fix is a possible workaround to a compiler bug when having the `-cuda` flag,
    which adds implicitly `-lcublas` and `-lcurand` on the link line without the link
    directories. This is the case in ectrans version 1.6.2.
    However already in the develop branch, with PR #261, we already removed this `-cuda` flag.
    
    A possible fix for 1.6.2 and any package using `-cuda` could instead be handled in a toolchain file e.g. by using
    CMAKE_Fortran_IMPLICIT_LINK_DIRECTORIES.

--- cmake/ectrans_find_cuda.cmake
+++ cmake/ectrans_find_cuda.cmake
@@ -21,18 +21,5 @@ macro( ectrans_find_cuda )
       ecbuild_info( "cuda arch               : [${CMAKE_CUDA_ARCHITECTURES}]" )
       ecbuild_info( "cublas                  : [${CUDA_cublas_LIBRARY}]" )
       ecbuild_info( "cufft                   : [${CUDA_cufft_LIBRARY}]" )
-
-      if( TARGET CUDA::cublas )
-         # When looking for CUDAToolkit, the various components are found using find_library
-         # which sets the target to the full path, but the parent directory is not added
-         # to the target's LINK_DIRECTORIES property. Whilst in principle this should be enough
-         # to link to the CUDA libraries, as the full path appears on the link line, it seems
-         # there is a transient dependency somewhere that also requires the directory path to be set.
-         # This symptom only manifests on systems where libcudart lives in a separate location to
-         # libcublas. It is unclear whether this is an nvhpc or a cmake bug, but nevertheless manually
-         # specifying the link directory here should be harmless.
-         cmake_path(GET CUDA_cublas_LIBRARY PARENT_PATH _cublas_path)
-         target_link_directories( CUDA::cublas INTERFACE ${_cublas_path} )
-      endif()
     endif()
 endmacro()
