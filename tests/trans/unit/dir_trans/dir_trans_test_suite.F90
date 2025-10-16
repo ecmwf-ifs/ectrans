b69b1879cb7b2bb9aca53e2bf97c881ce77e1067
Sam Hatfield <samuel.hatfield@ecmwf.int>
2025-10-16 09:35:25 +0000

    Fix indentation

--- tests/trans/unit/dir_trans/dir_trans_test_suite.F90
+++ tests/trans/unit/dir_trans/dir_trans_test_suite.F90
@@ -33,36 +33,36 @@ CONTAINS
 
 ! Approximate equality check for reals
 ELEMENTAL LOGICAL FUNCTION APPROX_EQ(A, B, TOL) RESULT(RET)
-    REAL(KIND=JPRB), INTENT(IN) :: A
-    REAL(KIND=JPRB), INTENT(IN) :: B
-    REAL(KIND=JPRB), INTENT(IN), OPTIONAL :: TOL
+  REAL(KIND=JPRB), INTENT(IN) :: A
+  REAL(KIND=JPRB), INTENT(IN) :: B
+  REAL(KIND=JPRB), INTENT(IN), OPTIONAL :: TOL
 
-    IF (PRESENT(TOL)) THEN
-        RET = ABS(A - B) < TOL
-    ELSE
-        RET = ABS(A - B) < TOLERANCE
-    END IF
+  IF (PRESENT(TOL)) THEN
+    RET = ABS(A - B) < TOL
+  ELSE
+    RET = ABS(A - B) < TOLERANCE
+  END IF
 END FUNCTION APPROX_EQ
 
 ! Setup fixture (octahedral grid)
 SUBROUTINE SETUP_TEST(KSPEC2, KGPTOT)
-    INTEGER(KIND=JPIM), INTENT(OUT) :: KSPEC2
-    INTEGER(KIND=JPIM), INTENT(OUT) :: KGPTOT
+  INTEGER(KIND=JPIM), INTENT(OUT) :: KSPEC2
+  INTEGER(KIND=JPIM), INTENT(OUT) :: KGPTOT
 
-    INTEGER(KIND=JPIM) :: ILOEN(NDGL)
-    INTEGER(KIND=JPIM) :: I
+  INTEGER(KIND=JPIM) :: ILOEN(NDGL)
+  INTEGER(KIND=JPIM) :: I
 
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
 
-    ! Define octahedral grid
-    DO I = 1, TRUNCATION + 1
-        ILOEN(I) = 20 + 4 * I
-        ILOEN(NDGL - I + 1) = ILOEN(I)
-    END DO
+  ! Define octahedral grid
+  DO I = 1, TRUNCATION + 1
+    ILOEN(I) = 20 + 4 * I
+    ILOEN(NDGL - I + 1) = ILOEN(I)
+  END DO
 
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN)
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN)
 
-    CALL TRANS_INQ(KSPEC2=KSPEC2, KGPTOT=KGPTOT)
+  CALL TRANS_INQ(KSPEC2=KSPEC2, KGPTOT=KGPTOT)
 END SUBROUTINE SETUP_TEST
 
 !---------------------------------------------------------------------------------------------------
@@ -76,213 +76,213 @@ END SUBROUTINE SETUP_TEST
 
 ! Test DIR_TRANS with call mode 1 and just one scalar field
 INTEGER FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_1_SCALAR_1() RESULT(RET) BIND(C)
-    REAL(KIND=JPRB), ALLOCATABLE :: ZGP(:,:,:), ZSPSCALAR(:,:)
-    INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
+  REAL(KIND=JPRB), ALLOCATABLE :: ZGP(:,:,:), ZSPSCALAR(:,:)
+  INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
 
-    CALL SETUP_TEST(NSPEC2, NGPTOT)
+  CALL SETUP_TEST(NSPEC2, NGPTOT)
 
-    ! Initialise arrays
-    ALLOCATE(ZGP(NGPTOT,1,1))
-    ALLOCATE(ZSPSCALAR(1,NSPEC2))
+  ! Initialise arrays
+  ALLOCATE(ZGP(NGPTOT,1,1))
+  ALLOCATE(ZSPSCALAR(1,NSPEC2))
 
-    ! Set all of the input to one
-    ZGP(:,:,:) = 1.0_JPRB
+  ! Set all of the input to one
+  ZGP(:,:,:) = 1.0_JPRB
 
-    CALL DIR_TRANS(PGP=ZGP, PSPSCALAR=ZSPSCALAR)
+  CALL DIR_TRANS(PGP=ZGP, PSPSCALAR=ZSPSCALAR)
 
-    ! Check only the (0,0) mode (global mean) is one and all the rest are zero
-    IF (APPROX_EQ(ZSPSCALAR(1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSCALAR(1,2:), 0.0_JPRB))) THEN
-        RET = 0
-    ELSE
-        RET = 1
-    END IF
+  ! Check only the (0,0) mode (global mean) is one and all the rest are zero
+  IF (APPROX_EQ(ZSPSCALAR(1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSCALAR(1,2:), 0.0_JPRB))) THEN
+    RET = 0
+  ELSE
+    RET = 1
+  END IF
 
-    DEALLOCATE(ZSPSCALAR, ZGP)
-    CALL TRANS_END
+  DEALLOCATE(ZSPSCALAR, ZGP)
+  CALL TRANS_END
 END FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_1_SCALAR_1
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test DIR_TRANS with call mode 1 and 1-level wind fields
 INTEGER FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_1_WIND_1() RESULT(RET) BIND(C)
-    REAL(KIND=JPRB), ALLOCATABLE :: ZGP(:,:,:), ZSPVOR(:,:), ZSPDIV(:,:)
-    INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
-    REAL(KIND=JPRB) :: TEMP_TOLERANCE
+  REAL(KIND=JPRB), ALLOCATABLE :: ZGP(:,:,:), ZSPVOR(:,:), ZSPDIV(:,:)
+  INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
+  REAL(KIND=JPRB) :: TEMP_TOLERANCE
 
-    CALL SETUP_TEST(NSPEC2, NGPTOT)
+  CALL SETUP_TEST(NSPEC2, NGPTOT)
 
-    ! Initialise arrays
-    ALLOCATE(ZGP(NGPTOT,2,1))
-    ALLOCATE(ZSPVOR(1,NSPEC2))
-    ALLOCATE(ZSPDIV(1,NSPEC2))
+  ! Initialise arrays
+  ALLOCATE(ZGP(NGPTOT,2,1))
+  ALLOCATE(ZSPVOR(1,NSPEC2))
+  ALLOCATE(ZSPDIV(1,NSPEC2))
 
-    ! Set all of the input to one
-    ZGP(:,:,:) = 1.0_JPRB
+  ! Set all of the input to one
+  ZGP(:,:,:) = 1.0_JPRB
 
-    CALL DIR_TRANS(PGP=ZGP, PSPVOR=ZSPVOR, PSPDIV=ZSPDIV)
+  CALL DIR_TRANS(PGP=ZGP, PSPVOR=ZSPVOR, PSPDIV=ZSPDIV)
 
-    ! We pass in constant U and V so vorticity and divergence should both be zero
-    ! TODO: for some reason the double precision computation of vorticity and divergence gives
-    ! errors more like single precision. This possibly indicates a hard-coded single-precision
-    ! value somewhere in the double-precision code path. For now we use a higher tolerance.
-    IF (JPRB == JPRD) THEN
-        TEMP_TOLERANCE = 1E7_JPRD * TOLERANCE
-    ELSE
-        TEMP_TOLERANCE = TOLERANCE
-    END IF
-    IF (ALL(APPROX_EQ(ZSPVOR(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
-      & ALL(APPROX_EQ(ZSPDIV(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE))) THEN
-        RET = 0
-    ELSE
-        RET = 1
-    END IF
+  ! We pass in constant U and V so vorticity and divergence should both be zero
+  ! TODO: for some reason the double precision computation of vorticity and divergence gives
+  ! errors more like single precision. This possibly indicates a hard-coded single-precision
+  ! value somewhere in the double-precision code path. For now we use a higher tolerance.
+  IF (JPRB == JPRD) THEN
+    TEMP_TOLERANCE = 1E7_JPRD * TOLERANCE
+  ELSE
+    TEMP_TOLERANCE = TOLERANCE
+  END IF
+  IF (ALL(APPROX_EQ(ZSPVOR(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
+    & ALL(APPROX_EQ(ZSPDIV(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE))) THEN
+    RET = 0
+  ELSE
+    RET = 1
+  END IF
 
-    DEALLOCATE(ZSPDIV, ZSPVOR, ZGP)
-    CALL TRANS_END
+  DEALLOCATE(ZSPDIV, ZSPVOR, ZGP)
+  CALL TRANS_END
 END FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_1_WIND_1
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test DIR_TRANS with call mode 1 and 1-level wind fields and 1 scalar field
 INTEGER FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_1_WIND_1_SCALAR_1() RESULT(RET) BIND(C)
-    REAL(KIND=JPRB), ALLOCATABLE :: ZGP(:,:,:), ZSPVOR(:,:), ZSPDIV(:,:), ZSPSCALAR(:,:)
-    INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
-    REAL(KIND=JPRB) :: TEMP_TOLERANCE
+  REAL(KIND=JPRB), ALLOCATABLE :: ZGP(:,:,:), ZSPVOR(:,:), ZSPDIV(:,:), ZSPSCALAR(:,:)
+  INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
+  REAL(KIND=JPRB) :: TEMP_TOLERANCE
 
-    CALL SETUP_TEST(NSPEC2, NGPTOT)
+  CALL SETUP_TEST(NSPEC2, NGPTOT)
 
-    ! Initialise arrays
-    ALLOCATE(ZGP(NGPTOT,3,1))
-    ALLOCATE(ZSPVOR(1,NSPEC2))
-    ALLOCATE(ZSPDIV(1,NSPEC2))
-    ALLOCATE(ZSPSCALAR(1,NSPEC2))
+  ! Initialise arrays
+  ALLOCATE(ZGP(NGPTOT,3,1))
+  ALLOCATE(ZSPVOR(1,NSPEC2))
+  ALLOCATE(ZSPDIV(1,NSPEC2))
+  ALLOCATE(ZSPSCALAR(1,NSPEC2))
 
-    ! Set all of the input to one
-    ZGP(:,:,:) = 1.0_JPRB
+  ! Set all of the input to one
+  ZGP(:,:,:) = 1.0_JPRB
 
-    CALL DIR_TRANS(PGP=ZGP, PSPVOR=ZSPVOR, PSPDIV=ZSPDIV, PSPSCALAR=ZSPSCALAR)
+  CALL DIR_TRANS(PGP=ZGP, PSPVOR=ZSPVOR, PSPDIV=ZSPDIV, PSPSCALAR=ZSPSCALAR)
 
-    ! We pass in constant U and V so vorticity and divergence should both be zero
-    ! As for the scalar, check only the (0,0) mode (global mean) is one and all the rest are zero
-    ! TODO: for some reason the double precision computation of vorticity and divergence gives
-    ! errors more like single precision. This possibly indicates a hard-coded single-precision
-    ! value somewhere in the double-precision code path. For now we use a higher tolerance.
-    IF (JPRB == JPRD) THEN
-        TEMP_TOLERANCE = 1E7_JPRD * TOLERANCE
-    ELSE
-        TEMP_TOLERANCE = TOLERANCE
-    END IF
-    IF (ALL(APPROX_EQ(ZSPVOR(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
-      & ALL(APPROX_EQ(ZSPDIV(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
-      & APPROX_EQ(ZSPSCALAR(1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSCALAR(1,2:), 0.0_JPRB))) THEN
-        RET = 0
-    ELSE
-        RET = 1
-    END IF
+  ! We pass in constant U and V so vorticity and divergence should both be zero
+  ! As for the scalar, check only the (0,0) mode (global mean) is one and all the rest are zero
+  ! TODO: for some reason the double precision computation of vorticity and divergence gives
+  ! errors more like single precision. This possibly indicates a hard-coded single-precision
+  ! value somewhere in the double-precision code path. For now we use a higher tolerance.
+  IF (JPRB == JPRD) THEN
+    TEMP_TOLERANCE = 1E7_JPRD * TOLERANCE
+  ELSE
+    TEMP_TOLERANCE = TOLERANCE
+  END IF
+  IF (ALL(APPROX_EQ(ZSPVOR(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
+    & ALL(APPROX_EQ(ZSPDIV(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
+    & APPROX_EQ(ZSPSCALAR(1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSCALAR(1,2:), 0.0_JPRB))) THEN
+    RET = 0
+  ELSE
+    RET = 1
+  END IF
 
-    DEALLOCATE(ZSPSCALAR, ZSPDIV, ZSPVOR, ZGP)
-    CALL TRANS_END
+  DEALLOCATE(ZSPSCALAR, ZSPDIV, ZSPVOR, ZGP)
+  CALL TRANS_END
 END FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_1_WIND_1_SCALAR_1
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test DIR_TRANS with call mode 2 and just one "3A" scalar field
 INTEGER FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_2_PGP3A_1() RESULT(RET) BIND(C)
-    REAL(KIND=JPRB), ALLOCATABLE :: ZGP3A(:,:,:,:), ZSPSC3A(:,:,:)
-    INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
-    REAL(KIND=JPRB) :: TEMP_TOLERANCE
+  REAL(KIND=JPRB), ALLOCATABLE :: ZGP3A(:,:,:,:), ZSPSC3A(:,:,:)
+  INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
+  REAL(KIND=JPRB) :: TEMP_TOLERANCE
 
-    CALL SETUP_TEST(NSPEC2, NGPTOT)
+  CALL SETUP_TEST(NSPEC2, NGPTOT)
 
-    ! Initialise arrays
-    ALLOCATE(ZGP3A(NGPTOT,1,1,1))
-    ALLOCATE(ZSPSC3A(1,NSPEC2,1))
+  ! Initialise arrays
+  ALLOCATE(ZGP3A(NGPTOT,1,1,1))
+  ALLOCATE(ZSPSC3A(1,NSPEC2,1))
 
-    ! Set all of the input to one
-    ZGP3A(:,:,:,:) = 1.0_JPRB
+  ! Set all of the input to one
+  ZGP3A(:,:,:,:) = 1.0_JPRB
 
-    CALL DIR_TRANS(PGP3A=ZGP3A, PSPSC3A=ZSPSC3A)
+  CALL DIR_TRANS(PGP3A=ZGP3A, PSPSC3A=ZSPSC3A)
 
-    ! Check only the (0,0) mode (global mean) is one and all the rest are zero
-    IF (APPROX_EQ(ZSPSC3A(1,1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSC3A(1,2:,1), 0.0_JPRB))) THEN
-        RET = 0
-    ELSE
-        RET = 1
-    END IF
+  ! Check only the (0,0) mode (global mean) is one and all the rest are zero
+  IF (APPROX_EQ(ZSPSC3A(1,1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSC3A(1,2:,1), 0.0_JPRB))) THEN
+    RET = 0
+  ELSE
+    RET = 1
+  END IF
 
-    DEALLOCATE(ZSPSC3A, ZGP3A)
-    CALL TRANS_END
+  DEALLOCATE(ZSPSC3A, ZGP3A)
+  CALL TRANS_END
 END FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_2_PGP3A_1
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test DIR_TRANS with call mode 2 and just one "3B" scalar field
 INTEGER FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_2_PGP3B_1() RESULT(RET) BIND(C)
-    REAL(KIND=JPRB), ALLOCATABLE :: ZGP3B(:,:,:,:), ZSPSC3B(:,:,:)
-    INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
-    REAL(KIND=JPRB) :: TEMP_TOLERANCE
+  REAL(KIND=JPRB), ALLOCATABLE :: ZGP3B(:,:,:,:), ZSPSC3B(:,:,:)
+  INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
+  REAL(KIND=JPRB) :: TEMP_TOLERANCE
 
-    CALL SETUP_TEST(NSPEC2, NGPTOT)
+  CALL SETUP_TEST(NSPEC2, NGPTOT)
 
-    ! Initialise arrays
-    ALLOCATE(ZGP3B(NGPTOT,1,1,1))
-    ALLOCATE(ZSPSC3B(1,NSPEC2,1))
+  ! Initialise arrays
+  ALLOCATE(ZGP3B(NGPTOT,1,1,1))
+  ALLOCATE(ZSPSC3B(1,NSPEC2,1))
 
-    ! Set all of the input to one
-    ZGP3B(:,:,:,:) = 1.0_JPRB
+  ! Set all of the input to one
+  ZGP3B(:,:,:,:) = 1.0_JPRB
 
-    CALL DIR_TRANS(PGP3B=ZGP3B, PSPSC3B=ZSPSC3B)
+  CALL DIR_TRANS(PGP3B=ZGP3B, PSPSC3B=ZSPSC3B)
 
-    ! Check only the (0,0) mode (global mean) is one and all the rest are zero
-    IF (APPROX_EQ(ZSPSC3B(1,1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSC3B(1,2:,1), 0.0_JPRB))) THEN
-        RET = 0
-    ELSE
-        RET = 1
-    END IF
+  ! Check only the (0,0) mode (global mean) is one and all the rest are zero
+  IF (APPROX_EQ(ZSPSC3B(1,1,1), 1.0_JPRB) .AND. ALL(APPROX_EQ(ZSPSC3B(1,2:,1), 0.0_JPRB))) THEN
+    RET = 0
+  ELSE
+    RET = 1
+  END IF
 
-    DEALLOCATE(ZSPSC3B, ZGP3B)
-    CALL TRANS_END
+  DEALLOCATE(ZSPSC3B, ZGP3B)
+  CALL TRANS_END
 END FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_2_PGP3B_1
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test DIR_TRANS with call mode 2 and 1-level wind fields
 INTEGER FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_2_WIND_1() RESULT(RET) BIND(C)
-    REAL(KIND=JPRB), ALLOCATABLE :: ZGPUV(:,:,:,:), ZSPVOR(:,:), ZSPDIV(:,:)
-    INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
-    REAL(KIND=JPRB) :: TEMP_TOLERANCE
+  REAL(KIND=JPRB), ALLOCATABLE :: ZGPUV(:,:,:,:), ZSPVOR(:,:), ZSPDIV(:,:)
+  INTEGER(KIND=JPIM) :: NSPEC2, NGPTOT
+  REAL(KIND=JPRB) :: TEMP_TOLERANCE
 
-    CALL SETUP_TEST(NSPEC2, NGPTOT)
+  CALL SETUP_TEST(NSPEC2, NGPTOT)
 
-    ! Initialise arrays
-    ALLOCATE(ZGPUV(NGPTOT,1,2,1))
-    ALLOCATE(ZSPVOR(1,NSPEC2))
-    ALLOCATE(ZSPDIV(1,NSPEC2))
+  ! Initialise arrays
+  ALLOCATE(ZGPUV(NGPTOT,1,2,1))
+  ALLOCATE(ZSPVOR(1,NSPEC2))
+  ALLOCATE(ZSPDIV(1,NSPEC2))
 
-    ! Set all of the input to one
-    ZGPUV(:,:,:,:) = 1.0_JPRB
+  ! Set all of the input to one
+  ZGPUV(:,:,:,:) = 1.0_JPRB
 
-    CALL DIR_TRANS(PGPUV=ZGPUV, PSPVOR=ZSPVOR, PSPDIV=ZSPDIV)
+  CALL DIR_TRANS(PGPUV=ZGPUV, PSPVOR=ZSPVOR, PSPDIV=ZSPDIV)
 
-    ! We pass in constant U and V so vorticity and divergence should both be zero
-    ! As for the scalar, check only the (0,0) mode (global mean) is one and all the rest are zero
-    ! TODO: for some reason the double precision computation of vorticity and divergence gives
-    ! errors more like single precision. This possibly indicates a hard-coded single-precision
-    ! value somewhere in the double-precision code path. For now we use a higher tolerance.
-    IF (JPRB == JPRD) THEN
-        TEMP_TOLERANCE = 1E7_JPRD * TOLERANCE
-    ELSE
-        TEMP_TOLERANCE = TOLERANCE
-    END IF
-    IF (ALL(APPROX_EQ(ZSPVOR(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
-      & ALL(APPROX_EQ(ZSPDIV(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE))) THEN
-        RET = 0
-    ELSE
-        RET = 1
-    END IF
+  ! We pass in constant U and V so vorticity and divergence should both be zero
+  ! As for the scalar, check only the (0,0) mode (global mean) is one and all the rest are zero
+  ! TODO: for some reason the double precision computation of vorticity and divergence gives
+  ! errors more like single precision. This possibly indicates a hard-coded single-precision
+  ! value somewhere in the double-precision code path. For now we use a higher tolerance.
+  IF (JPRB == JPRD) THEN
+    TEMP_TOLERANCE = 1E7_JPRD * TOLERANCE
+  ELSE
+    TEMP_TOLERANCE = TOLERANCE
+  END IF
+  IF (ALL(APPROX_EQ(ZSPVOR(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE)) .AND. &
+    & ALL(APPROX_EQ(ZSPDIV(1,1:), 0.0_JPRB, TOL=TEMP_TOLERANCE))) THEN
+    RET = 0
+  ELSE
+    RET = 1
+  END IF
 
-    DEALLOCATE(ZSPDIV, ZSPVOR, ZGPUV)
-    CALL TRANS_END
+  DEALLOCATE(ZSPDIV, ZSPVOR, ZGPUV)
+  CALL TRANS_END
 END FUNCTION UNIT_TEST_DIR_TRANS_CALL_MODE_2_WIND_1
 
 !---------------------------------------------------------------------------------------------------
--- tests/trans/unit/setup_trans/setup_trans_test_suite.F90
+++ tests/trans/unit/setup_trans/setup_trans_test_suite.F90
@@ -32,127 +32,127 @@ CONTAINS
 
 ! Test SETUP_TRANS without first calling SETUP_TRANS0 - should fail
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_WITHOUT_SETUP_TRANS0() RESULT(RET) BIND(C)
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL)
-    RET = 0
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL)
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_WITHOUT_SETUP_TRANS0
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with regular lat-lon grid of 2*(TRUNCATION+1) latitudes
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_BASIC() RESULT(RET) BIND(C)
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL)
-    CALL TRANS_END
-    RET = 0
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL)
+  CALL TRANS_END
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_BASIC
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with an odd number of latitudes - should fail
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_ODD_NDGL() RESULT(RET) BIND(C)
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL - 1)
-    RET = 0
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL - 1)
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_ODD_NDGL
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with octahedral grid of 2*(TRUNCATION+1) latitudes 
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_OCTAHEDRAL() RESULT(RET) BIND(C)
-    INTEGER(KIND=JPIM) :: ILOEN(NDGL)
-    INTEGER(KIND=JPIM) :: I
+  INTEGER(KIND=JPIM) :: ILOEN(NDGL)
+  INTEGER(KIND=JPIM) :: I
 
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
 
-    ! Define octahedral grid
-    DO I = 1, TRUNCATION + 1
-        ILOEN(I) = 20 + 4 * I
-        ILOEN(NDGL - I + 1) = ILOEN(I)
-    END DO
+  ! Define octahedral grid
+  DO I = 1, TRUNCATION + 1
+    ILOEN(I) = 20 + 4 * I
+    ILOEN(NDGL - I + 1) = ILOEN(I)
+  END DO
 
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN)
-    CALL TRANS_END
-    RET = 0
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN)
+  CALL TRANS_END
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_OCTAHEDRAL
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with stretch factor passed
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_STRETCHING() RESULT(RET) BIND(C)
-    INTEGER(KIND=JPIM) :: ILOEN(NDGL)
-    INTEGER(KIND=JPIM) :: I
+  INTEGER(KIND=JPIM) :: ILOEN(NDGL)
+  INTEGER(KIND=JPIM) :: I
 
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
 
-    ! Define octahedral grid
-    DO I = 1, TRUNCATION + 1
-        ILOEN(I) = 20 + 4 * I
-        ILOEN(NDGL - I + 1) = ILOEN(I)
-    END DO
+  ! Define octahedral grid
+  DO I = 1, TRUNCATION + 1
+    ILOEN(I) = 20 + 4 * I
+    ILOEN(NDGL - I + 1) = ILOEN(I)
+  END DO
 
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, PSTRET=2.0_JPRD)
-    CALL TRANS_END
-    RET = 0
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, PSTRET=2.0_JPRD)
+  CALL TRANS_END
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_STRETCHING
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with fast Legendre transform 
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_FLT() RESULT(RET) BIND(C)
-    INTEGER(KIND=JPIM) :: ILOEN(NDGL)
-    INTEGER(KIND=JPIM) :: I
+  INTEGER(KIND=JPIM) :: ILOEN(NDGL)
+  INTEGER(KIND=JPIM) :: I
 
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
 
-    ! Define octahedral grid
-    DO I = 1, TRUNCATION + 1
-        ILOEN(I) = 20 + 4 * I
-        ILOEN(NDGL - I + 1) = ILOEN(I)
-    END DO
+  ! Define octahedral grid
+  DO I = 1, TRUNCATION + 1
+    ILOEN(I) = 20 + 4 * I
+    ILOEN(NDGL - I + 1) = ILOEN(I)
+  END DO
 
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, LDUSEFLT=.TRUE.)
-    CALL TRANS_END
-    RET = 0
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, LDUSEFLT=.TRUE.)
+  CALL TRANS_END
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_FLT
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with all fields passed to FFTW at ocne
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_ALL_FFTW() RESULT(RET) BIND(C)
-    INTEGER(KIND=JPIM) :: ILOEN(NDGL)
-    INTEGER(KIND=JPIM) :: I
+  INTEGER(KIND=JPIM) :: ILOEN(NDGL)
+  INTEGER(KIND=JPIM) :: I
 
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
 
-    ! Define octahedral grid
-    DO I = 1, TRUNCATION + 1
-        ILOEN(I) = 20 + 4 * I
-        ILOEN(NDGL - I + 1) = ILOEN(I)
-    END DO
+  ! Define octahedral grid
+  DO I = 1, TRUNCATION + 1
+    ILOEN(I) = 20 + 4 * I
+    ILOEN(NDGL - I + 1) = ILOEN(I)
+  END DO
 
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, LD_ALL_FFTW=.TRUE.)
-    CALL TRANS_END
-    RET = 0
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, LD_ALL_FFTW=.TRUE.)
+  CALL TRANS_END
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_ALL_FFTW
 
 !---------------------------------------------------------------------------------------------------
 
 ! Test SETUP_TRANS with Belusov algorithm
 INTEGER FUNCTION UNIT_TEST_SETUP_TRANS_BELUSOV() RESULT(RET) BIND(C)
-    INTEGER(KIND=JPIM) :: ILOEN(NDGL)
-    INTEGER(KIND=JPIM) :: I
+  INTEGER(KIND=JPIM) :: ILOEN(NDGL)
+  INTEGER(KIND=JPIM) :: I
 
-    CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
+  CALL SETUP_TRANS0(LDMPOFF=.TRUE.)
 
-    ! Define octahedral grid
-    DO I = 1, TRUNCATION + 1
-        ILOEN(I) = 20 + 4 * I
-        ILOEN(NDGL - I + 1) = ILOEN(I)
-    END DO
+  ! Define octahedral grid
+  DO I = 1, TRUNCATION + 1
+    ILOEN(I) = 20 + 4 * I
+    ILOEN(NDGL - I + 1) = ILOEN(I)
+  END DO
 
-    CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, LDUSERPNM=.TRUE.)
-    CALL TRANS_END
-    RET = 0
+  CALL SETUP_TRANS(KSMAX=TRUNCATION, KDGL=NDGL, KLOEN=ILOEN, LDUSERPNM=.TRUE.)
+  CALL TRANS_END
+  RET = 0
 END FUNCTION UNIT_TEST_SETUP_TRANS_BELUSOV
 
 !---------------------------------------------------------------------------------------------------
