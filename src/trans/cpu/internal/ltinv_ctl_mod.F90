79b06b12a76b47413d9418ca88dc40587594aeaa
DJDavies2 <david.davies@metoffice.gov.uk>
2025-11-12 12:32:13 +0000

    Patches for nag

--- src/trans/cpu/internal/ltinv_ctl_mod.F90
+++ src/trans/cpu/internal/ltinv_ctl_mod.F90
@@ -13,6 +13,143 @@ CONTAINS
 SUBROUTINE LTINV_CTL(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,&
  & PSPVOR,PSPDIV,PSPSCALAR,&
  & PSPSC3A,PSPSC3B,PSPSC2,&
+ & KFLDPTRUV,KFLDPTRSC)
+
+!**** *LTINV_CTL* - Control routine for inverse Legandre transform.
+
+!     Purpose.
+!     --------
+!        Control routine for the inverse LEGENDRE transform
+
+!**   Interface.
+!     ----------
+!     CALL INV_TRANS_CTL(...)
+!     KF_OUT_LT    - number of fields coming out from inverse LT
+!     KF_UV        - local number of spectral u-v fields
+!     KF_SCALARS   - local number of scalar spectral fields
+!     KF_SCDERS    - local number of derivatives of scalar spectral fields
+!     PSPVOR(:,:)  - spectral vorticity (input)
+!     PSPDIV(:,:)  - spectral divergence (input)
+!     PSPSCALAR(:,:) - spectral scalarvalued fields (input)
+!     KFLDPTRUV(:) - field pointer array for vor./div.
+!     KFLDPTRSC(:) - field pointer array for PSPSCALAR
+
+!     Method.
+!     -------
+
+!     Externals.
+!     ----------
+!
+
+!     Author.
+!     -------
+!        Mats Hamrud *ECMWF*
+
+!     Modifications.
+!     --------------
+!        Original : 00-06-03
+
+!     ------------------------------------------------------------------
+
+USE PARKIND1   ,ONLY : JPIM     ,JPRB
+
+USE TPM_GEN    ,ONLY : LALLOPERM
+USE TPM_TRANS  ,ONLY : FOUBUF, FOUBUF_IN
+USE TPM_DISTR  ,ONLY : D
+USE TPM_FLT    ,ONLY : S
+
+USE LTINV_MOD  ,ONLY : LTINV
+USE TRMTOL_MOD ,ONLY : TRMTOL
+
+IMPLICIT NONE
+
+INTEGER(KIND=JPIM),INTENT(IN) :: KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPVOR(:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPDIV(:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPSCALAR(:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPSC3A(:,:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPSC3B(:,:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPSC2(:,:)
+INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRUV(:)
+INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRSC(:)
+
+INTEGER(KIND=JPIM) :: JM,IM,IBLEN,ILEI2,IDIM1
+
+!     ------------------------------------------------------------------
+
+write(0, '(a)') "in ltinv_ctl 1"
+CALL GSTATS(102,0)
+ILEI2 = 8*KF_UV + 2*KF_SCALARS + 2*KF_SCDERS
+IDIM1 = 2*KF_OUT_LT
+IBLEN = D%NLENGT0B*2*KF_OUT_LT
+IF (ALLOCATED(FOUBUF)) THEN
+  IF (MAX(1,IBLEN) > SIZE(FOUBUF)) THEN
+    DEALLOCATE(FOUBUF)
+    ALLOCATE(FOUBUF(MAX(1,IBLEN)))
+  ENDIF
+ELSE
+  ALLOCATE(FOUBUF(MAX(1,IBLEN)))
+ENDIF
+IF (ALLOCATED(FOUBUF_IN)) THEN
+  IF (MAX(1,IBLEN) > SIZE(FOUBUF_IN)) THEN
+    DEALLOCATE(FOUBUF_IN)
+    ALLOCATE(FOUBUF_IN(MAX(1,IBLEN)))
+  ENDIF
+ELSE
+  ALLOCATE(FOUBUF_IN(MAX(1,IBLEN)))
+  FOUBUF_IN(:) = 0
+ENDIF
+
+write(0, '(a)') "in ltinv_ctl 2"
+! Following switch necessary when latlon grids are used with different increments in NS and EW direction.
+! Otherwise unassigned values will appear in output. This is very likely a bug (ATLAS-149)
+IF (S%LDLL) THEN
+  FOUBUF_IN(:) = 0
+ENDIF
+
+write(0, '(a)') "in ltinv_ctl 2.1"
+IF(KF_OUT_LT > 0) THEN
+write(0, '(a)') "in ltinv_ctl 2.2"
+  CALL GSTATS(1647,0)
+
+  !!!WARNING!!! Duplication of code besides the FSPGL_PROC argument.
+              ! It seems that gfortran 10 does not retain the value
+              ! of FSPGL_PROC within the OMP region.
+write(0, '(a,i0)') "in ltinv_ctl 2.3, d % nump = ", d % nump
+#ifndef NAG
+  !$OMP PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JM,IM)
+#endif
+  DO JM=1,D%NUMP
+    IM = D%MYMS(JM)
+write(0, '(a,i0,a,i0)') "in ltinv_ctl 2.3, jm = ", jm, " im = ", im
+    CALL LTINV(IM,JM,KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,ILEI2,IDIM1,&
+     & PSPVOR,PSPDIV,PSPSCALAR ,&
+     & PSPSC3A,PSPSC3B,PSPSC2 , &
+     & KFLDPTRUV,KFLDPTRSC)
+  ENDDO
+#ifndef NAG
+  !$OMP END PARALLEL DO
+#endif
+write(0, '(a)') "in ltinv_ctl 2.4"
+  CALL GSTATS(1647,1)
+write(0, '(a)') "in ltinv_ctl 2.5"
+ENDIF
+
+write(0, '(a)') "in ltinv_ctl 3"
+CALL GSTATS(102,1)
+
+CALL GSTATS(152,0)
+CALL TRMTOL(FOUBUF_IN,FOUBUF,2*KF_OUT_LT)
+CALL GSTATS(152,1)
+IF (.NOT.LALLOPERM) DEALLOCATE(FOUBUF_IN)
+!     ------------------------------------------------------------------
+write(0, '(a)') "in ltinv_ctl 4"
+
+END SUBROUTINE LTINV_CTL
+
+SUBROUTINE LTINV_CTL_PROC(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,&
+ & PSPVOR,PSPDIV,PSPSCALAR,&
+ & PSPSC3A,PSPSC3B,PSPSC2,&
  & KFLDPTRUV,KFLDPTRSC,FSPGL_PROC)
 
 !**** *LTINV_CTL* - Control routine for inverse Legandre transform.
@@ -75,7 +212,6 @@ REAL(KIND=JPRB) ,OPTIONAL, INTENT(IN)  :: PSPSC2(:,:)
 INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRUV(:)
 INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRSC(:)
 EXTERNAL  FSPGL_PROC
-OPTIONAL  FSPGL_PROC
 
 INTEGER(KIND=JPIM) :: JM,IM,IBLEN,ILEI2,IDIM1
 
@@ -115,27 +251,15 @@ IF(KF_OUT_LT > 0) THEN
   !!!WARNING!!! Duplication of code besides the FSPGL_PROC argument.
               ! It seems that gfortran 10 does not retain the value
               ! of FSPGL_PROC within the OMP region.
-  IF( PRESENT(FSPGL_PROC) ) THEN
-    !$OMP PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JM,IM)
-    DO JM=1,D%NUMP
-      IM = D%MYMS(JM)
-      CALL LTINV(IM,JM,KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,ILEI2,IDIM1,&
-       & PSPVOR,PSPDIV,PSPSCALAR ,&
-       & PSPSC3A,PSPSC3B,PSPSC2 , &
-       & KFLDPTRUV,KFLDPTRSC,FSPGL_PROC)
-    ENDDO
-    !$OMP END PARALLEL DO
-  ELSE
-    !$OMP PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JM,IM)
-    DO JM=1,D%NUMP
-      IM = D%MYMS(JM)
-      CALL LTINV(IM,JM,KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,ILEI2,IDIM1,&
-       & PSPVOR,PSPDIV,PSPSCALAR ,&
-       & PSPSC3A,PSPSC3B,PSPSC2 , &
-       & KFLDPTRUV,KFLDPTRSC)
-    ENDDO
-    !$OMP END PARALLEL DO
-  ENDIF
+  !$OMP PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JM,IM)
+  DO JM=1,D%NUMP
+    IM = D%MYMS(JM)
+    CALL LTINV(IM,JM,KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,ILEI2,IDIM1,&
+     & PSPVOR,PSPDIV,PSPSCALAR ,&
+     & PSPSC3A,PSPSC3B,PSPSC2 , &
+     & KFLDPTRUV,KFLDPTRSC,FSPGL_PROC)
+  ENDDO
+  !$OMP END PARALLEL DO
   CALL GSTATS(1647,1)
 ENDIF
 
@@ -147,5 +271,5 @@ CALL GSTATS(152,1)
 IF (.NOT.LALLOPERM) DEALLOCATE(FOUBUF_IN)
 !     ------------------------------------------------------------------
 
-END SUBROUTINE LTINV_CTL
+END SUBROUTINE LTINV_CTL_PROC
 END MODULE LTINV_CTL_MOD
--- src/trans/cpu/internal/ltinv_ctlad_mod.F90
+++ src/trans/cpu/internal/ltinv_ctlad_mod.F90
@@ -13,6 +13,114 @@ CONTAINS
 SUBROUTINE LTINV_CTLAD(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,&
  & PSPVOR,PSPDIV,PSPSCALAR,&
  & PSPSC3A,PSPSC3B,PSPSC2,&
+ & KFLDPTRUV,KFLDPTRSC)
+
+!**** *LTINV_CTLAD* - Control routine for inverse Legandre transform - adj.
+
+!     Purpose.
+!     --------
+!     Control routine for the inverse LEGENDRE transform
+
+!**   Interface.
+!     ----------
+!     CALL INV_TRANS_CTL(...)
+!     KF_OUT_LT    - number of fields coming out from inverse LT
+!     KF_UV        - local number of spectral u-v fields
+!     KF_SCALARS   - local number of scalar spectral fields
+!     KF_SCDERS    - local number of derivatives of scalar spectral fields
+!     PSPVOR(:,:)  - spectral vorticity (input)
+!     PSPDIV(:,:)  - spectral divergence (input)
+!     PSPSCALAR(:,:) - spectral scalarvalued fields (input)
+!     KFLDPTRUV(:) - field pointer array for vor./div.
+!     KFLDPTRSC(:) - field pointer array for PSPSCALAR
+
+!     Method.
+!     -------
+
+!     Externals.
+!     ----------
+!
+
+!     Author.
+!     -------
+!        Mats Hamrud *ECMWF*
+
+!     Modifications.
+!     --------------
+!        Original : 00-06-03
+
+!     ------------------------------------------------------------------
+USE PARKIND1  ,ONLY : JPIM     ,JPRB
+
+USE TPM_GEN         ,ONLY : LALLOPERM
+!USE TPM_DIM
+USE TPM_TRANS       ,ONLY : FOUBUF, FOUBUF_IN
+USE TPM_DISTR       ,ONLY : D
+USE LTINVAD_MOD     ,ONLY : LTINVAD
+USE TRLTOM_MOD      ,ONLY : TRLTOM
+
+IMPLICIT NONE
+
+INTEGER(KIND=JPIM),INTENT(IN) :: KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT) :: PSPVOR(:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT) :: PSPDIV(:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT) :: PSPSCALAR(:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT)  :: PSPSC3A(:,:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT)  :: PSPSC3B(:,:,:)
+REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT)  :: PSPSC2(:,:)
+INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRUV(:)
+INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRSC(:)
+
+INTEGER(KIND=JPIM) :: JM,IM,IBLEN,ILEI2,IDIM1
+
+!     ------------------------------------------------------------------
+
+ILEI2 = 8*KF_UV + 2*KF_SCALARS + 2*KF_SCDERS
+IDIM1 = 2*KF_OUT_LT
+IBLEN = D%NLENGT0B*2*KF_OUT_LT
+IF (ALLOCATED(FOUBUF_IN)) THEN
+  IF (MAX(1,IBLEN) > SIZE(FOUBUF_IN)) THEN
+    DEALLOCATE(FOUBUF_IN)
+    ALLOCATE(FOUBUF_IN(MAX(1,IBLEN)))
+  ENDIF
+ELSE
+  ALLOCATE(FOUBUF_IN(MAX(1,IBLEN)))
+ENDIF
+CALL GSTATS(180,0)
+CALL TRLTOM(FOUBUF,FOUBUF_IN,2*KF_OUT_LT)
+CALL GSTATS(180,1)
+IF (.NOT.LALLOPERM) DEALLOCATE(FOUBUF)
+
+CALL GSTATS(104,0)
+CALL GSTATS(1648,0)
+IF(KF_OUT_LT > 0) THEN
+! Bug in gcc <= 10.2, see https://github.com/ecmwf-ifs/ectrans/issues/20
+#if !(defined(__GFORTRAN__) && __GNUC__ == 10 && __GNUC_MINOR__ <= 2)
+!$OMP PARALLEL DO SCHEDULE(DYNAMIC,1) PRIVATE(JM,IM)
+#endif
+  DO JM=1,D%NUMP
+    IM = D%MYMS(JM)
+    CALL LTINVAD(IM,JM,KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,ILEI2,IDIM1,&
+     & PSPVOR,PSPDIV,PSPSCALAR,&
+     & PSPSC3A,PSPSC3B,PSPSC2 , &
+     & KFLDPTRUV,KFLDPTRSC)
+  ENDDO
+#if !(defined(__GFORTRAN__) && __GNUC__ == 10 && __GNUC_MINOR__ <= 2)
+!$OMP END PARALLEL DO
+#endif
+ENDIF
+CALL GSTATS(1648,1)
+
+IF (.NOT.LALLOPERM) DEALLOCATE(FOUBUF_IN)
+CALL GSTATS(104,1)
+
+!     ------------------------------------------------------------------
+
+END SUBROUTINE LTINV_CTLAD
+
+SUBROUTINE LTINV_CTLAD_PROC(KF_OUT_LT,KF_UV,KF_SCALARS,KF_SCDERS,&
+ & PSPVOR,PSPDIV,PSPSCALAR,&
+ & PSPSC3A,PSPSC3B,PSPSC2,&
  & KFLDPTRUV,KFLDPTRSC,FSPGL_PROC)
 
 !**** *LTINV_CTLAD* - Control routine for inverse Legandre transform - adj.
@@ -73,7 +181,6 @@ REAL(KIND=JPRB) ,OPTIONAL, INTENT(OUT)  :: PSPSC2(:,:)
 INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRUV(:)
 INTEGER(KIND=JPIM),OPTIONAL,INTENT(IN) :: KFLDPTRSC(:)
 EXTERNAL  FSPGL_PROC
-OPTIONAL  FSPGL_PROC
 
 INTEGER(KIND=JPIM) :: JM,IM,IBLEN,ILEI2,IDIM1
 
@@ -120,5 +227,5 @@ CALL GSTATS(104,1)
 
 !     ------------------------------------------------------------------
 
-END SUBROUTINE LTINV_CTLAD
+END SUBROUTINE LTINV_CTLAD_PROC
 END MODULE LTINV_CTLAD_MOD
