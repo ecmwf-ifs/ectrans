d4e44ac5f76bb4bf01ba0042a9cd4c999fc15361
Sam Hatfield <samuel.hatfield@ecmwf.int>
2026-04-17 15:20:32 +0000

    Update CI NVHPC to 26.3

--- .github/tools/install-nvhpc.sh
+++ .github/tools/install-nvhpc.sh
@@ -12,7 +12,7 @@
 # See <https://creativecommons.org/publicdomain/zero/1.0/> for
 # details.
 
-version=25.11
+version=26.3
 
 TEMPORARY_FILES="${TMPDIR:-/tmp}"
 export NVHPC_INSTALL_DIR=$(pwd)/nvhpc-install
