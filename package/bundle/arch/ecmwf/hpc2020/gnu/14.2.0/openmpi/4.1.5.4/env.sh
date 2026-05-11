5cb4f5a1f04b511397d3d67ba31555e9778bd806
Sam Hatfield <samuel.hatfield@ecmwf.int>
2026-05-11 10:48:28 +0100

    Add license headers

--- package/bundle/arch/ecmwf/hpc2020/gnu/14.2.0/openmpi/4.1.5.4/env.sh
+++ package/bundle/arch/ecmwf/hpc2020/gnu/14.2.0/openmpi/4.1.5.4/env.sh
@@ -1,3 +1,11 @@
+# (C) Copyright 2026- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 # Source me to get the correct configure/build/run environment
 
 # Store tracing and disable (module is *way* too verbose)
