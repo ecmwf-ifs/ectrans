f620e8de6d60134266c69aae3646fde8e300f786
Sam Hatfield <samuel.hatfield@ecmwf.int>
2026-09-10 12:24:25 +0000

    Remove m=0 special treatment for LEINV

--- src/trans/gpu/internal/leinv_mod.F90
+++ src/trans/gpu/internal/leinv_mod.F90
@@ -66,7 +66,7 @@ CONTAINS
     END ASSOCIATE
   END SUBROUTINE LEINV_STRIDES
 
-  SUBROUTINE LEINV(ALLOCATOR,PIA,ZINP,ZINP0,ZOUTS,ZOUTA,ZOUTS0,ZOUTA0,KF_LEG)
+  SUBROUTINE LEINV(ALLOCATOR,PIA,ZINP,ZOUTS,ZOUTA,KF_LEG)
     !**** *LEINV* - Inverse Legendre transform.
 
     !     Purpose.
@@ -130,7 +130,6 @@ CONTAINS
     REAL(KIND=JPRB),    INTENT(IN)  :: PIA(:,:,:)
     INTEGER(KIND=JPIM), INTENT(IN)  :: KF_LEG
     REAL(KIND=JPRBT), POINTER, INTENT(OUT) :: ZINP(:), ZOUTS(:), ZOUTA(:)
-    REAL(KIND=JPRD), POINTER, INTENT(OUT) :: ZINP0(:), ZOUTS0(:), ZOUTA0(:)
     TYPE(BUFFERED_ALLOCATOR), INTENT(IN) :: ALLOCATOR
 
     !     LOCAL
@@ -139,7 +138,6 @@ CONTAINS
     INTEGER(KIND=JPIM)  :: KM, KMLOC, IA, IS, JK, J, IMLOC0(1)
     INTEGER(KIND=JPIM)  :: IOUT_STRIDES0
     INTEGER(KIND=JPIM)  :: IIN_STRIDES0
-    INTEGER(KIND=JPIM)  :: IOUT0_STRIDES0
     INTEGER(KIND=JPIM)  :: IIN0_STRIDES0
 
     REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
@@ -168,19 +166,19 @@ CONTAINS
     !*       1.1      PREPARATIONS.
 
     CALL LEINV_STRIDES(KF_LEG, IOUT_STRIDES0=IOUT_STRIDES0, IIN_STRIDES0=IIN_STRIDES0, &
-      &                IOUT0_STRIDES0=IOUT0_STRIDES0, IIN0_STRIDES0=IIN0_STRIDES0)
+      &                IIN0_STRIDES0=IIN0_STRIDES0)
 
 
 #ifdef OMPGPU
     !$OMP TARGET DATA &
     !$OMP&              MAP(PRESENT,ALLOC:D,D_MYMS,D_NUMP) &
-    !$OMP&              MAP(PRESENT,ALLOC:ZINP,ZOUTS,ZOUTA,ZINP0,ZOUTS0,ZOUTA0) &
+    !$OMP&              MAP(PRESENT,ALLOC:ZINP,ZOUTS,ZOUTA) &
     !$OMP&              MAP(PRESENT,ALLOC:ZAA,ZAS,PIA) &
     !$OMP&              MAP(PRESENT,ALLOC:R,R_NSMAX,D_OFFSETS_GEMM2)
 #endif
 #ifdef ACCGPU
     !$ACC DATA PRESENT(D,D_MYMS,D_NUMP) &
-    !$ACC&     PRESENT(ZINP,ZOUTS,ZOUTA,ZINP0,ZOUTS0,ZOUTA0) &
+    !$ACC&     PRESENT(ZINP,ZOUTS,ZOUTA) &
     !$ACC&     PRESENT(ZAA,ZAS,PIA) &
     !$ACC&     PRESENT(R,R_NSMAX,D_OFFSETS_GEMM2)
 #endif
@@ -216,39 +214,21 @@ CONTAINS
       DO JK=1,2*KF_LEG
         KM =  D_MYMS(KMLOC)
         IA  = 1+MOD(R_NSMAX-KM+2,2)
-        IF(KM /= 0)THEN
 #ifdef ACCGPU
-          !$ACC LOOP SEQ
+        !$ACC LOOP SEQ
 #endif
-          DO J=1,(R_NSMAX-KM+2)/2
-            ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=PIA(JK,IA+1+(J-1)*2,KMLOC)
-          ENDDO
-          ! those are only needed with tensor cores (zinp might contain NaNs!)
+        DO J=1,(R_NSMAX-KM+2)/2
+          ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=PIA(JK,IA+1+(J-1)*2,KMLOC)
+        ENDDO
+        ! those are only needed with tensor cores (zinp might contain NaNs!)
 #if defined(USE_CUTLASS) && defined(USE_CUTLASS_3XTF32)
-          !$ACC LOOP SEQ
-          DO J=(R_NSMAX-KM+2)/2+1,ALIGN((R_NSMAX-KM+2)/2,A)
-            ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=0
-          ENDDO
+        !$ACC LOOP SEQ
+        DO J=(R_NSMAX-KM+2)/2+1,ALIGN((R_NSMAX-KM+2)/2,A)
+          ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=0
+        ENDDO
 #endif
-        ELSEIF (MOD((JK-1),2) == 0) THEN
-          ! every other field is sufficient because Im(KM=0) == 0
-#ifdef ACCGPU
-          !$ACC LOOP SEQ
-#endif
-          DO J=1,(R_NSMAX+2)/2
-            ZINP0((JK-1)/2+1+(J-1)*IIN0_STRIDES0) = PIA(JK,IA+1+(J-1)*2,KMLOC)
-          ENDDO
-          ! those are only needed with tensor cores (zinp might contain NaNs!)
-#if defined(USE_CUTLASS) && defined(USE_CUTLASS_3XTF32)
-          !$ACC LOOP SEQ
-          DO J=(R_NSMAX+2)/2+1,ALIGN((R_NSMAX+2)/2,A)
-            ZINP0((JK-1)/2+1+(J-1)*IIN0_STRIDES0) = 0
-          ENDDO
-#endif
-        ENDIF
       ENDDO
     ENDDO
-
 
     IF (LSYNC_TRANS) THEN
 #ifdef ACCGPU
@@ -260,32 +240,6 @@ CONTAINS
     ENDIF
     CALL GSTATS(424,0)
 
-    IMLOC0 = FINDLOC(D_MYMS,0)
-    IF (IMLOC0(1) > 0) THEN
-      ! compute m=0 in double precision
-#ifdef OMPGPU
-      !$OMP TARGET DATA USE_DEVICE_ADDR(ZAA0,ZINP0,ZOUTA0)
-#endif
-#ifdef ACCGPU
-      !$ACC HOST_DATA USE_DEVICE(ZAA0,ZINP0,ZOUTA0)
-#endif
-      CALL HIP_DGEMM_BATCHED( &
-        & 'N', 'T', &
-        & KF_LEG, G_NDGLU(0), (R_NSMAX+2)/2, &
-        & 1.0_JPRD, &
-        & C_LOC(ZINP0), IIN0_STRIDES0, 0, &
-        & C_LOC(ZAA0), SIZE(ZAA0,1), 0, &
-        & 0.0_JPRD, &
-        & C_LOC(ZOUTA0), IOUT0_STRIDES0, 0, &
-        & 1, HIP_STREAM, C_LOC(ALLOCATOR%PTR))
-#ifdef ACCGPU
-      !$ACC END HOST_DATA
-#endif
-#ifdef OMPGPU
-      !$OMP END TARGET DATA
-#endif
-   ENDIF
-
     DO KMLOC=1,D_NUMP
       KM = D_MYMS(KMLOC)
       KS(KMLOC) = (R_NSMAX-KM+2)/2
@@ -294,15 +248,12 @@ CONTAINS
       BOFFSETS(KMLOC) = D%OFFSETS_GEMM_MATRIX(KMLOC)
       COFFSETS(KMLOC) = IOUT_STRIDES0*D_OFFSETS_GEMM1(KMLOC)
     ENDDO
-    IF(IMLOC0(1) > 0) THEN
-      NS(IMLOC0(1)) = 0
-      KS(IMLOC0(1)) = 0
-    ENDIF
+
 #ifdef OMPGPU
-      !$OMP TARGET DATA USE_DEVICE_ADDR(ZAA,ZINP,ZOUTA)
+    !$OMP TARGET DATA USE_DEVICE_ADDR(ZAA,ZINP,ZOUTA)
 #endif
 #ifdef ACCGPU
-      !$ACC HOST_DATA USE_DEVICE(ZAA,ZINP,ZOUTA)
+    !$ACC HOST_DATA USE_DEVICE(ZAA,ZINP,ZOUTA)
 #endif
     CALL HIP_GEMM( &
         & NCUR_RESOL, 11, & ! unique identifier
@@ -315,10 +266,10 @@ CONTAINS
         & C_LOC(ZOUTA), IOUT_STRIDES0, COFFSETS, &
         & D_NUMP, HIP_STREAM, C_LOC(ALLOCATOR%PTR))
 #ifdef ACCGPU
-      !$ACC END HOST_DATA
+    !$ACC END HOST_DATA
 #endif
 #ifdef OMPGPU
-      !$OMP END TARGET DATA
+    !$OMP END TARGET DATA
 #endif
 
     IF (LSYNC_TRANS) THEN
@@ -361,35 +312,19 @@ CONTAINS
       DO JK=1,2*KF_LEG
         KM =  D_MYMS(KMLOC)
         IS  = 1+MOD(R_NSMAX-KM+1,2)
-        IF(KM /= 0) THEN
 #ifdef ACCGPU
-          !$ACC LOOP SEQ
+        !$ACC LOOP SEQ
 #endif
-          DO J=1,(R_NSMAX-KM+3)/2
-            ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=PIA(JK,IS+1+(J-1)*2,KMLOC)
-          ENDDO
+        DO J=1,(R_NSMAX-KM+3)/2
+          ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=PIA(JK,IS+1+(J-1)*2,KMLOC)
+        ENDDO
 #if defined(USE_CUTLASS) && defined(USE_CUTLASS_3XTF32)
-          ! those are only needed with tensor cores (zinp might contain NaNs!)
-          !$ACC LOOP SEQ
-          DO J=(R_NSMAX-KM+3)/2+1,ALIGN((R_NSMAX-KM+3)/2,A)
-            ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=0
-          ENDDO
+        ! those are only needed with tensor cores (zinp might contain NaNs!)
+        !$ACC LOOP SEQ
+        DO J=(R_NSMAX-KM+3)/2+1,ALIGN((R_NSMAX-KM+3)/2,A)
+          ZINP(JK+(J-1)*IIN_STRIDES0+D_OFFSETS_GEMM2(KMLOC)*IIN_STRIDES0)=0
+        ENDDO
 #endif
-        ELSEIF (MOD((JK-1),2) == 0) THEN
-#ifdef ACCGPU
-          !$ACC LOOP SEQ
-#endif
-          DO J=1,(R_NSMAX+3)/2
-            ZINP0((JK-1)/2+1+(J-1)*IIN0_STRIDES0) = PIA(JK,IS+1+(J-1)*2,KMLOC)
-          ENDDO
-          ! those are only needed with tensor cores (zinp might contain NaNs!)
-#if defined(USE_CUTLASS) && defined(USE_CUTLASS_3XTF32)
-          !$ACC LOOP SEQ
-          DO J=(R_NSMAX+3)/2+1,ALIGN((R_NSMAX+3)/2,A)
-            ZINP0((JK-1)/2+1+(J-1)*IIN0_STRIDES0) = 0
-          ENDDO
-#endif
-        ENDIF
       ENDDO
     ENDDO
 
@@ -403,30 +338,6 @@ CONTAINS
     ENDIF
     CALL GSTATS(424,0)
 
-    IF (IMLOC0(1) > 0) THEN
-#ifdef OMPGPU
-      !$OMP TARGET DATA USE_DEVICE_ADDR(ZAS0,ZINP0,ZOUTS0)
-#endif
-#ifdef ACCGPU
-      !$ACC HOST_DATA USE_DEVICE(ZAS0,ZINP0,ZOUTS0)
-#endif
-      CALL HIP_DGEMM_BATCHED( &
-        & 'N', 'T', &
-        & KF_LEG, G_NDGLU(0), (R_NSMAX+3)/2, &
-        & 1.0_JPRD, &
-        & C_LOC(ZINP0), IIN0_STRIDES0, 0, &
-        & C_LOC(ZAS0), SIZE(ZAS0,1), 0, &
-        & 0.0_JPRD, &
-        & C_LOC(ZOUTS0), IOUT0_STRIDES0, 0, &
-        & 1, HIP_STREAM, C_LOC(ALLOCATOR%PTR))
-#ifdef ACCGPU
-      !$ACC END HOST_DATA
-#endif
-#ifdef OMPGPU
-      !$OMP END TARGET DATA
-#endif
-    ENDIF
-
     DO KMLOC=1,D_NUMP
       KM = D_MYMS(KMLOC)
       KS(KMLOC) = (R_NSMAX-KM+3)/2
@@ -435,10 +346,7 @@ CONTAINS
       BOFFSETS(KMLOC) = D%OFFSETS_GEMM_MATRIX(KMLOC)
       COFFSETS(KMLOC) = IOUT_STRIDES0*D_OFFSETS_GEMM1(KMLOC)
     ENDDO
-    IF(IMLOC0(1) > 0) THEN
-      NS(IMLOC0(1)) = 0
-      KS(IMLOC0(1)) = 0
-    ENDIF
+
 #ifdef OMPGPU
     !$OMP TARGET DATA USE_DEVICE_ADDR(ZAS,ZINP,ZOUTS)
 #endif
