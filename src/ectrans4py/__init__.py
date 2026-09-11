15c5f099be769016342bcc6c29917596978cf862
Sam Hatfield <samuel.hatfield@ecmwf.int>
2026-09-11 15:23:40 +0100

    Make latitude array double precision

--- src/ectrans4py/__init__.py
+++ src/ectrans4py/__init__.py
@@ -142,7 +142,7 @@ def get_legendre_assets(KSIZEJ, KTRUNC, KSLOEN, KSPOLEGL, KLOEN, KNUMMAXRESOL):
              (np.int64, None, IN),
              (np.int64, (KSLOEN,), OUT),
              (_REAL, (KSLOEN,), OUT),
-             (_REAL, (KSLOEN,), OUT),
+             (np.float64, (KSLOEN,), OUT),
              (_REAL, (KSLOEN//2,KSPOLEGL), OUT)],
             None)
 
