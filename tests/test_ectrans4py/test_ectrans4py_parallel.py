25f4ba75a1997d3d239051c4056816783d1b1260
Sam Hatfield <samuel.hatfield@ecmwf.int>
2026-09-14 10:02:56 +0100

    Implement test base class

--- tests/test_ectrans4py/test_ectrans4py_parallel.py
+++ tests/test_ectrans4py/test_ectrans4py_parallel.py
@@ -9,16 +9,37 @@
 from unittest import TestCase
 import ectrans4py
 import platform
+from mpi4py import MPI
 
-system = platform.system()
-if system == "Linux":
-    ectrans4py.init_env(unlimited_stack=True)
-elif system == "Darwin":
-    ectrans4py.init_env(unlimited_stack=False)
-else:
-    raise NotImplementedError("ectrans4py does not support Windows")
 
-class TestInverse(TestCase):
+class Ectrans4pyTestBase(TestCase):
+    _env_initialised = False
+
+    @classmethod
+    def setUpClass(cls):
+        if not Ectrans4pyTestBase._env_initialised:
+            system = platform.system()
+            if system == "Linux":
+                ectrans4py.init_env(unlimited_stack=True)
+            elif system == "Darwin":
+                ectrans4py.init_env(unlimited_stack=False)
+            else:
+                raise NotImplementedError("ectrans4py does not support Windows")
+            Ectrans4pyTestBase._env_initialised = True
+
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+        if rank == 0:
+            print(f"Testing with {size} MPI processes")
+        ectrans4py.mpl_init4py()
+
+    @classmethod
+    def tearDownClass(cls):
+        ectrans4py.mpl_end4py()
+
+
+class TestInverse(Ectrans4pyTestBase):
     def test_scalar(self):
         return True
 
@@ -28,28 +49,28 @@ class TestInverse(TestCase):
     def test_uv(self):
         return True
 
-class TestDirect(TestCase):
+class TestDirect(Ectrans4pyTestBase):
     def test_scalar(self):
         return True
 
     def test_uv(self):
         return True
 
-class Distribute(TestCase):
+class Distribute(Ectrans4pyTestBase):
     def dist_spec(self):
         return True
 
     def dist_grid(self):
         return True
 
-class Gather(TestCase):
+class Gather(Ectrans4pyTestBase):
     def gath_spec(self):
         return True
 
     def gath_grid(self):
         return True
 
-class Norm(TestCase):
+class Norm(Ectrans4pyTestBase):
     def specnorm(self):
         return True
 
